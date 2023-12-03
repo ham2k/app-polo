fd4ad24de0f1d2775e2776e1f9b495d84b9201ab
Sebastian Delmont <sd@notso.net>
2023-12-03 11:49:39 -0500

    JSX Intentation in esling

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,7 @@ module.exports = {
   overrides: [],
   rules: {
     'prettier/prettier': 0,
+    'react/jsx-indent': [2, 2, { checkAttributes: false, indentLogicalExpressions: true }],
     'react/prop-types': 'off',
     'no-debugger': 'warn',
     'multiline-ternary': 'off'
