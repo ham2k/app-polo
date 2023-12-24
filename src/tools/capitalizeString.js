7f9f82fc6252533251d261b4dbf6f0f8d2d0ceda
Sebastian Delmont <sd@notso.net>
2023-12-24 09:59:53 -0500

    Capitalization

--- src/tools/capitalizeString.js
+++ src/tools/capitalizeString.js
@@ -1,29 +1,66 @@
-const WORD_OR_HYPENED_SEPARATOR_REGEX = /([^\s-]+?)([\s-]+|\s*$)/g
+const MIXED_CASE_REGEX = /[A-Z][a-z]/g
+const ALL_UPPER_CASE_REGEX = /^[A-Z]+$/g
+const WORD_OR_HYPENED_SEPARATOR_REGEX = /([^\s-.]+?)([\s-.]+|\s*$)/g
+const LETTERS_ONLY_REGEX = /^[a-z]+$/
+const PERIOD_REGEX = /\./g
 
 const CAPTITALIZATION_EXCEPTIONS = {
-  i: 'II',
+  iaru: 'IARU',
+  ii: 'II',
   iii: 'III',
   iv: 'IV',
+  ios: 'iOS',
+  iota: 'IOTA',
+  iphone: 'iPhone',
+  ipad: 'iPad',
   arrl: 'ARRL',
   ares: 'ARES',
-  fcc: 'FCC',
-  qrz: 'QRZ',
   clublog: 'ClubLog',
+  fcc: 'FCC',
+  flexradio: 'FlexRadio',
+  gridtracker: 'GridTracker',
   ham2k: 'Ham2K',
-  nasa: 'NASA'
+  nasa: 'NASA',
+  pota: 'POTA',
+  sota: 'SOTA',
+  qro: 'QRO',
+  qrp: 'QRP',
+  qrz: 'QRZ',
+  qsl: 'QSL',
+  qso: 'QSO',
+  qth: 'QTH',
+  youtube: 'YouTube'
 }
 
 export function capitalizeString (str, options = {}) {
-  const { mode } = options
+  let { content = 'text', force = true } = options
 
+  str = str?.trim()
   if (!str) return str
-  return str.trim().toLowerCase().replace(WORD_OR_HYPENED_SEPARATOR_REGEX, (match, word, separator) => {
-    if (CAPTITALIZATION_EXCEPTIONS[word]) {
-      return CAPTITALIZATION_EXCEPTIONS[word] + separator
-    } else if (word.length === 1 && mode === 'name') {
-      return word.toUpperCase() + '.' + separator
+
+  // If we're not forcing capitalization, only do it if the string is already mixed case
+  if (!force && !str.match(MIXED_CASE_REGEX)) {
+    force = true
+  }
+
+  return str.replace(WORD_OR_HYPENED_SEPARATOR_REGEX, (match, word, separator) => {
+    if (force) {
+      word = word.toLowerCase()
+      if (CAPTITALIZATION_EXCEPTIONS[word]) {
+        return CAPTITALIZATION_EXCEPTIONS[word] + separator
+      } else if (word.length === 1 && content === 'name' && word.match(LETTERS_ONLY_REGEX)) {
+        // Initials
+        return word.toUpperCase() + (separator.match(PERIOD_REGEX) ? '' : '.') + separator
+      } else {
+        return word.charAt(0).toUpperCase() + word.slice(1) + separator
+      }
     } else {
-      return word.charAt(0).toUpperCase() + word.slice(1) + separator
+      if (word.length === 1 && content === 'name' && word.match(ALL_UPPER_CASE_REGEX)) {
+        // When respecting existing case, only add periods to initials if they are already capitalized
+        return word.toUpperCase() + (separator.match(PERIOD_REGEX) ? '' : '.') + separator
+      } else {
+        return word + separator
+      }
     }
   })
 }
