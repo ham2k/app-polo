7fdaacad0be3d055c2f3cccf6316216abaac4543
Sebastian Delmont <sd@notso.net>
2024-02-02 09:48:39 -0500

    Better QSO List performance

--- src/screens/OperationScreen/OpLoggingTab/OpLoggingTab.jsx
+++ src/screens/OperationScreen/OpLoggingTab/OpLoggingTab.jsx
@@ -1,148 +1,66 @@
-import React, { useCallback, useEffect, useRef, useState } from 'react'
+import React, { createContext, useEffect, useRef, useState } from 'react'
 
 import { View } from 'react-native'
-import { useDispatch, useSelector } from 'react-redux'
+import { useSelector } from 'react-redux'
 
-import { selectOperation, setOperationData } from '../../../store/operations'
+import { selectOperation } from '../../../store/operations'
 import { useThemedStyles } from '../../../styles/tools/useThemedStyles'
 import LoggingPanel from './components/LoggingPanel'
 import QSOList from './components/QSOList'
-import { addQSO, selectQSOs } from '../../../store/qsos'
+import { selectQSOs } from '../../../store/qsos'
 import { selectSettings } from '../../../store/settings'
-import cloneDeep from 'clone-deep'
-import { qsoKey } from '@ham2k/lib-qson-tools'
 
-function prepareNewQSO (operation, settings) {
+export const OperationContext = createContext()
+
+function prepareStyles (themeStyles, themeColor) {
   return {
-    band: operation.band,
-    mode: operation.mode,
-    _is_new: true
+    ...themeStyles,
+    root: {
+      borderTopColor: themeStyles.theme.colors[`${themeColor}Light`],
+      borderTopWidth: 1,
+      backgroundColor: themeStyles.theme.colors[`${themeColor}Container`]
+    },
+    input: {
+      backgroundColor: themeStyles.theme.colors.background,
+      color: themeStyles.theme.colors.onBackground,
+      paddingHorizontal: themeStyles.oneSpace
+    }
   }
 }
 
-function prepareExistingQSO (qso) {
-  const clone = cloneDeep(qso)
-  clone._originalKey = qso.key
-  clone._is_new = false
-  return clone
-}
+const flexOne = { flex: 1 }
+const flexZero = { flex: 0 }
 
 export default function OpLoggingTab ({ navigation, route }) {
-  const styles = useThemedStyles((baseStyles) => {
-    return {
-      ...baseStyles,
-      input: {
-        backgroundColor: baseStyles.theme.colors.background,
-        color: baseStyles.theme.colors.onBackground,
-        paddingHorizontal: baseStyles.oneSpace
-      }
-    }
-  })
+  const styles = useThemedStyles((baseStyles) => prepareStyles(baseStyles))
 
-  const dispatch = useDispatch()
   const operation = useSelector(selectOperation(route.params.operation.uuid))
-  const settings = useSelector(selectSettings)
   const qsos = useSelector(selectQSOs(route.params.operation.uuid))
+  const settings = useSelector(selectSettings)
 
-  const listRef = useRef()
-  const mainFieldRef = useRef()
-  const focusedFieldRef = useRef()
+  const [selectedKey, setSelectedKey] = useState(undefined)
+  const [lastKey, setLastKey] = useState(undefined)
+
+  const operationContext = {
+    operation,
+    qsos,
+    settings,
+    selectedKey,
+    setSelectedKey,
+    lastKey,
+    setLastKey
+  }
 
   // Set navigation title
   useEffect(() => {
     navigation.setOptions({ title: `${qsos.length} ${qsos.length !== 1 ? 'QSOs' : 'QSO'}`, iconName: 'radio' })
   }, [navigation, qsos])
-
-  // When the lastQSO changes, scroll to it
-  const [lastQSO, setLastQSO] = useState()
-  useEffect(() => {
-    setTimeout(() => {
-      if (lastQSO) {
-        const i = qsos.findIndex((qso) => qso.key === lastQSO.ket)
-        if (i > -1) {
-          listRef.current?.scrollToIndex({ index: i, animated: true })
-        } else {
-          listRef.current?.scrollToEnd()
-        }
-      } else {
-        listRef.current?.scrollToEnd()
-      }
-    }, 0)
-  }, [listRef, qsos, lastQSO])
-
-  const [qsoQueue, setQSOQueue] = useState([])
-  const [currentQSO, setCurrentQSO] = useState(undefined)
-
-  // When there is no current QSO, pop one from the queue or create a new one
-  useEffect(() => {
-    if (!currentQSO) {
-      if (qsoQueue.length > 0) {
-        const nextQSO = qsoQueue.pop()
-        setCurrentQSO(nextQSO)
-        setQSOQueue(qsoQueue)
-      } else {
-        setCurrentQSO(prepareNewQSO(operation, settings))
-      }
-      if (mainFieldRef?.current) {
-        mainFieldRef.current.focus()
-      }
-    }
-  }, [qsoQueue, currentQSO, operation, settings])
-
-  const handleSelectQSO = useCallback((qso) => {
-    if (qso) {
-      if (currentQSO?._is_new) setQSOQueue([...qsoQueue, currentQSO])
-      setCurrentQSO(prepareExistingQSO(qso))
-      if (mainFieldRef?.current) {
-        mainFieldRef.current.focus()
-      }
-    } else {
-      setCurrentQSO(undefined) // blanking the current QSO will trigger the useEffect above to prepare the next one
-    }
-  }, [currentQSO, qsoQueue])
-
-  // Log (or update) a QSO
-  const logQSO = useCallback((qso) => {
-    if (qso._is_new) {
-      delete qso._is_new
-    }
-
-    qso.mode = qso.mode || operation.mode
-    qso.freq = qso.freq || operation.freq
-
-    if (!qso.startOnMillis) qso.startOnMillis = new Date()
-    qso.startOn = new Date(qso.startOnMillis).toISOString()
-    if (qso.endOnMillis) qso.endOn = new Date(qso.endOnMillis).toISOString()
-
-    qso.our = qso.our || {}
-    qso.our.call = qso.our.call || operation.stationCall || settings.operatorCall
-    qso.our.sent = qso.our.sent || (operation.mode === 'CW' ? '599' : '59')
-
-    qso.their = qso.their || {}
-    qso.their.sent = qso.their.sent || (operation.mode === 'CW' ? '599' : '59')
-
-    qso.key = qsoKey(qso)
-
-    dispatch(addQSO({ uuid: operation.uuid, qso }))
-
-    setLastQSO(qso)
-    setCurrentQSO(undefined)
-  }, [dispatch, operation, settings])
-
-  const handleOperationChange = useCallback((attributes) => {
-    dispatch(setOperationData({ uuid: operation.uuid, ...attributes }))
-  }, [dispatch, operation.uuid])
-
   return (
-    <View style={{ flex: 1 }}>
-      <QSOList qsos={qsos} styles={styles} style={{ flex: 1 }} listRef={listRef} onSelect={handleSelectQSO} selected={currentQSO} />
-      <LoggingPanel
-        qso={currentQSO} setQSO={setCurrentQSO}
-        operation={operation} qsos={qsos} settings={settings}
-        onAccept={logQSO} onOperationChange={handleOperationChange}
-        mainFieldRef={mainFieldRef} focusedFieldRef={focusedFieldRef}
-        themeColor={currentQSO?._is_new ? 'tertiary' : 'secondary'} style={{ flex: 0 }}
-      />
-    </View>
+    <OperationContext.Provider value={operationContext}>
+      <View style={flexOne}>
+        <QSOList style={flexOne} styles={styles} qsos={qsos} selectedKey={selectedKey} setSelectedKey={setSelectedKey} lastKey={lastKey} />
+        <LoggingPanel style={flexZero} />
+      </View>
+    </OperationContext.Provider>
   )
 }
--- src/screens/OperationScreen/OpLoggingTab/components/QSOItem.jsx
+++ src/screens/OperationScreen/OpLoggingTab/components/QSOItem.jsx
@@ -1,74 +1,45 @@
 import React from 'react'
-import { Text, TouchableRipple } from 'react-native-paper'
+import { Icon, Text, TouchableRipple } from 'react-native-paper'
 
 import { fmtShortTimeZulu } from '../../../../tools/timeFormats'
 import { View } from 'react-native'
 
-const REF_LABELS = {
-  potaActivation: false,
-  pota: 'POTA',
-  sota: 'SOTA',
-  wwff: 'WWFF',
-  bota: 'BOTA',
-  iota: 'IOTA'
+import { activityIndex } from '../../activities'
+
+export function guessItemHeight (qso, styles) {
+  return styles.compactRow.height + styles.compactRow.borderBottomWidth
 }
 
-export default class QSOItem extends React.PureComponent {
-  render () {
-    const { qso, onPress, styles, selected, extendedWidth } = this.props
+const REFS_TO_INCLUDE = {
+  pota: true
+}
 
-    const multiRow = qso.notes || (qso?.refs?.filter(ref => REF_LABELS[ref.type]).length > 0)
-
-    return (
-      <TouchableRipple onPress={onPress} style={{ backgroundColor: selected ? styles.theme.colors.secondaryLight : undefined }}>
-        <View>
-          <View style={[styles.compactRow, { flexDirection: 'row', width: '100%' }, multiRow ? { borderBottomWidth: 0, paddingBottom: 0 } : {}]}>
-            <View style={{ flex: 0, marginLeft: 0, alignContent: 'right', minWidth: styles.oneSpace * 2 }}>
-              <Text style={[styles.text.numbers, { textAlign: 'right' }]}>{qso._number}</Text>
-            </View>
-            <View style={{ flex: 0, minWidth: styles.oneSpace * 6, marginLeft: styles.oneSpace }}>
-              <Text style={[styles.text.numbers, styles.text.lighter, { textAlign: 'right' }]}>{fmtShortTimeZulu(qso.startOnMillis)}</Text>
-            </View>
-            <View style={{ flex: 0, minWidth: styles.oneSpace * 4, marginLeft: styles.oneSpace, alignContent: 'right' }}>
-              <Text style={[styles.text.numbers, styles.text.lighter]}>{qso.freq ?? qso.band}</Text>
-            </View>
-            <View style={{ flex: 1, marginLeft: styles.oneSpace, minWidth: styles.oneSpace * 7 }}>
-              <Text style={[styles.text.callsign, { fontWeight: 'bold' }]}>{qso.their?.call ?? '?'}</Text>
-            </View>
-            {qso?.their?.exchange ? (
-              <>
-                {extendedWidth && (
-                  <View style={{ flex: 0, marginLeft: styles.oneSpace, minWidth: styles.oneSpace * 3, alignContent: 'right' }}>
-                    <Text style={[styles.text.numbers, { textAlign: 'right' }]}>{qso?.our?.sent}{'  '}{qso?.their?.sent}</Text>
-                  </View>
-                )}
-
-                <View style={{ flex: 0, marginLeft: styles.oneSpace, minWidth: styles.oneSpace * 3, alignContent: 'right' }}>
-                  <Text style={[styles.text.callsign, { textAlign: 'right' }]}>{qso?.their?.exchange}</Text>
-                </View>
-              </>
-            ) : (
-              <View style={{ flex: 0, marginLeft: styles.oneSpace, minWidth: styles.oneSpace * 3, alignContent: 'right' }}>
-                <Text style={[styles.text.numbers, { textAlign: 'right' }]}>{qso?.our?.sent}{'  '}{qso?.their?.sent}</Text>
-              </View>
+const QSOItem = React.memo(function QSOItem ({ qso, onPress, styles, selected, extendedWidth }) {
+  return (
+    <TouchableRipple onPress={() => onPress && onPress({ qso })} style={{ backgroundColor: selected ? styles.theme.colors.secondaryLight : undefined }}>
+      <View style={styles.compactRow}>
+        <Text style={styles.fields.number}>{qso._number}</Text>
+        <Text style={styles.fields.time}>{fmtShortTimeZulu(qso.startOnMillis)}</Text>
+        <Text style={styles.fields.freq}>{qso.freq ?? qso.band}</Text>
+        <Text style={styles.fields.call}>{qso.their?.call ?? '?'}</Text>
+        {qso.notes && (
+          <Icon source="note-outline" size={styles.oneSpace * 2} style={styles.fields.icon} />
+        )}
+        {(qso.refs || []).filter(ref => REFS_TO_INCLUDE[ref.type]).map(ref => ({ ref, activity: activityIndex[ref.type] })).map(({ ref, activity }, i) => (
+          <Icon key={i} source={activity?.icon} size={styles.oneSpace * 2} style={styles.fields.icon} />
+        ))}
+        {qso?.their?.exchange ? (
+          <>
+            {extendedWidth && (
+              <Text style={styles.fields.signal}>{qso?.our?.sent}{'  '}{qso?.their?.sent}</Text>
             )}
-          </View>
-          {multiRow && (
-            <View style={[styles.compactRow, { flexDirection: 'column', width: '100%', paddingVertical: 0, paddingBottom: styles.halfSpace, minHeight: undefined }]}>
-              {qso.notes && (
-                <View style={{ flex: 2, marginLeft: styles.oneSpace * 8, flexDirection: 'row' }}>
-                  <Text style={{ fontWeight: '600', marginRight: styles.oneSpace }}>Notes:</Text><Text>{qso.notes}</Text>
-                </View>
-              )}
-              {(qso.refs || []).filter(ref => REF_LABELS[ref.type]).map((ref, i) => (
-                <View key={`${ref.type}-${i}`} style={{ flex: 2, marginLeft: styles.oneSpace * 8, flexDirection: 'row' }}>
-                  <Text style={{ fontWeight: '600', marginRight: styles.oneSpace }}>{REF_LABELS[ref.type]}:</Text><Text>{ref.ref}</Text>
-                </View>
-              ))}
-            </View>
-          )}
-        </View>
-      </TouchableRipple>
-    )
-  }
-}
+            <Text style={styles.fields.exchange}>{qso?.their?.exchange}</Text>
+          </>
+        ) : (
+          <Text style={styles.fields.signal}>{qso?.our?.sent}{'  '}{qso?.their?.sent}</Text>
+        )}
+      </View>
+    </TouchableRipple>
+  )
+})
+export default QSOItem
