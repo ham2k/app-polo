f9ffb2ccb06fb1d18673c6e49c26ce6758b414ee
Sebastian Delmont <sd@notso.net>
2024-02-04 15:11:33 -0500

    Cleanup

--- src/store/apiPOTA/potaData.js
+++ src/store/apiPOTA/potaData.js
@@ -3,32 +3,28 @@ import { registerDataFile } from '../dataFiles'
 
 import RNFetchBlob from 'react-native-blob-util'
 
+import packageJson from '../../../package.json'
+
 export const POTAAllParks = { byReference: {}, prefixByDXCCCode: {} }
 export function preparePOTAAllParksData () {
-  console.log('POTA Data')
   registerDataFile({
     key: 'pota-all-parks',
     name: 'POTA - All Parks',
     maxAgeInDays: 7,
     autoLoad: true,
     fetch: async () => {
-      // const url = 'https://sdelmont.s3.amazonaws.com/all_parks_ext.csv' //
       const url = 'https://pota.app/all_parks_ext.csv'
 
-      console.log('Fetching POTA Data')
-      const response = await RNFetchBlob.config({ fileCache: true }).fetch('GET', url)
-      console.log('Fetching body POTA Data', response.data)
+      const response = await RNFetchBlob.config({ fileCache: true }).fetch('GET', url, {
+        'User-Agent': `Ham2K Portable Logger/${packageJson.version}`
+      })
       const body = await RNFetchBlob.fs.readFile(response.data, 'utf8')
-      console.log('Parsing POTA Data', body.substring(0, 200))
 
       const parks = []
       const lines = body.split('\n')
-      console.log('-- lines', lines.length)
       const headers = parsePOTACSVRow(lines.shift())
       lines.forEach(line => {
         const row = parsePOTACSVRow(line, { headers })
-        if (parks.length % 1000 === 0) console.log('-- row', parks.length, row.reference)
-        if (parks.length === 0) console.log('CSV Row', row)
         parks.push({
           ref: row.reference,
           dxccCode: Number.parseInt(row.entityId, 10),
@@ -40,27 +36,22 @@ export function preparePOTAAllParksData () {
         })
       })
 
-      console.log('Loaded POTA Data')
-
       const activeParks = parks.filter(park => park.active)
 
       const data = {
         byReference: activeParks.reduce((obj, item) => Object.assign(obj, { [item.ref]: item }), {}),
         prefixByDXCCCode: parks.reduce((obj, item) => {
-          // if (!obj[item.dxccCode]) {
-          //   console.log('DXCC Code', { code: item.dxccCode, ref: item.ref, prefix: item.ref && item.ref.split('-')[0] })
-          // }
           if (!obj[item.dxccCode]) obj[item.dxccCode] = item.ref && item.ref.split('-')[0]
           return obj
         }, {}),
         version: fmtDateNice(new Date())
       }
 
+      RNFetchBlob.fs.unlink(response.data)
+
       return data
     },
     onLoad: (data) => {
-      console.log('POTA Loaded', data.prefixByDXCCCode)
-
       Object.assign(POTAAllParks, data)
     }
   })
