5e96e340af280a420cfd3db450c7d61aca77d8fb
Sebastian Delmont <sd@notso.net>
2024-02-22 09:27:33 -0500

    Show extended history in call info dialog

--- src/screens/OperationScreen/OpLoggingTab/components/LoggingPanel/CallInfoDialog.jsx
+++ src/screens/OperationScreen/OpLoggingTab/components/LoggingPanel/CallInfoDialog.jsx
@@ -1,8 +1,11 @@
-import React, { useCallback } from 'react'
+import React, { useCallback, useMemo } from 'react'
 import { Button, Chip, Dialog, Icon, Portal, Text } from 'react-native-paper'
 import { View, KeyboardAvoidingView, Image, Linking } from 'react-native'
 import { capitalizeString } from '../../../../../tools/capitalizeString'
 import { DXCC_BY_PREFIX } from '@ham2k/lib-dxcc-data'
+import { fmtDateTimeDynamic } from '../../../../../tools/timeFormats'
+
+const HISTORY_QSOS_TO_SHOW = 3
 
 export function CallInfoDialog ({
   visible, setVisible,
@@ -11,6 +14,41 @@ export function CallInfoDialog ({
   styles
 }) {
   const entity = DXCC_BY_PREFIX[guess?.entityPrefix]
+
+  const [thisOpTitle, thisOpQSOs, historyTitle, historyRecent, historyAndMore] = useMemo(() => {
+    const thisQs = (callHistory || []).filter(q => operation && q.operation === operation?.uuid)
+    const otherOps = (callHistory || []).filter(q => q.operation !== operation?.uuid)
+
+    const recent = otherOps?.slice(0, HISTORY_QSOS_TO_SHOW) || []
+    let thisTitle
+    let title
+    let andMore
+
+    if (thisQs?.length === 1) {
+      thisTitle = 'One QSO in this operation'
+    } else if (thisQs?.length > 1) {
+      thisTitle = `${thisQs.length} QSOs in this operation`
+    } else {
+      thisTitle = ''
+    }
+
+    if (otherOps?.length === 1) {
+      title = 'One previous QSO'
+    } else if (otherOps?.length > 1) {
+      title = `${otherOps.length} previous QSOs`
+    } else {
+      title = 'No previous QSOs'
+    }
+
+    if (otherOps?.length <= HISTORY_QSOS_TO_SHOW) {
+      andMore = ''
+    } else if (otherOps?.length > HISTORY_QSOS_TO_SHOW) {
+      andMore = `… and ${otherOps.length - HISTORY_QSOS_TO_SHOW} more`
+    } else {
+      andMore = ''
+    }
+    return [thisTitle, thisQs, title, recent, andMore]
+  }, [callHistory, operation])
 
   const handleDone = useCallback(() => {
     setVisible(false)
@@ -68,6 +106,38 @@ export function CallInfoDialog ({
                   )}
                 </View>
               </View>
+              {thisOpTitle && (
+                <View style={{ flexDirection: 'column' }}>
+                  <Text variant="bodyLarge" style={{ fontWeight: 'bold' }}>
+                    {thisOpTitle}
+                  </Text>
+                  {thisOpQSOs.map((q, i) => (
+                    <View key={i} style={{ flexDirection: 'row', gap: styles.oneSpace }}>
+                      <Text style={{}}>{q.band}</Text>
+                      <Text style={{}}>{q.mode}</Text>
+                      <Text style={{}}>{fmtDateTimeDynamic(q.startOnMillis)}</Text>
+                    </View>
+                  ))}
+                </View>
+              )}
+              <View style={{ flexDirection: 'column' }}>
+                <Text variant="bodyLarge" style={{ fontWeight: 'bold' }}>
+                  {historyTitle}
+                </Text>
+                {historyRecent.map((q, i) => (
+                  <View key={i} style={{ flexDirection: 'row', gap: styles.oneSpace }}>
+                    <Text style={{}}>{q.band}</Text>
+                    <Text style={{}}>{q.mode}</Text>
+                    <Text style={{}}>{fmtDateTimeDynamic(q.startOnMillis)}</Text>
+                  </View>
+                ))}
+                {historyAndMore && (
+                  <Text style={{}}>
+                    {historyAndMore}
+                  </Text>
+                )}
+              </View>
+
             </Dialog.Content>
             <Dialog.Actions style={{ justifyContent: 'space-between' }}>
               <View />
