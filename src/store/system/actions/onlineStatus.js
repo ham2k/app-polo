9ec99a02a80ef0e121500b60b9a0995555dc4088
Sebastian Delmont <sd@notso.net>
2024-02-28 07:33:42 -0500

    Cleanup

--- src/store/system/actions/onlineStatus.js
+++ src/store/system/actions/onlineStatus.js
@@ -5,12 +5,9 @@ let unsubscribe
 
 export const setupOnlineStatusMonitoring = () => (dispatch) => {
   unsubscribe = addEventListener(state => {
-    console.log('Online Status', state)
     if (state.isConnected) {
       dispatch(actions.setOnline(state.isInternetReachable === null ? true : state.isInternetReachable))
-      console.log('Online?', state.isInternetReachable === null ? true : state.isInternetReachable)
     } else {
-      console.log('Offline!')
       dispatch(actions.setOnline(false))
     }
   })
