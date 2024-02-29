d283567c9d1a36168f95c5a7747f9fa55e51570c
Sebastian Delmont <sd@notso.net>
2024-02-29 09:47:05 -0500

    sota notes

--- src/screens/OperationScreen/activities/SOTAActivity.jsx
+++ src/screens/OperationScreen/activities/SOTAActivity.jsx
@@ -6,6 +6,30 @@ import { Text, TextInput } from 'react-native-paper'
 import { ActivitySettingsDialog } from '../components/ActivitySettingsDialog'
 import { replaceRefs, stringToRefs } from '../../../tools/refTools'
 
+/*
+https://sotastore.blob.core.windows.net/docs/SOTA-General-Rules-June-2022.pdf
+
+At least one QSO must be made from the Summit to qualify an activation.
+
+In order for the activation to qualify for the points attributed to that Summit, a minimum of four
+QSOs must be made, each of which must be with a different station.
+
+QSOs must comprise an exchange of callsigns and signal reports; it is strongly recommended that
+the summit identifier be given during each contact.
+
+Activator points accrue to the operator regardless of the callsign used. The operator must
+be entitled to use the callsign. Multiple operators of the same station may claim activator
+points. Each individual operator must make the minimum number of QSOs stated above
+in order to claim Activator points.
+
+The Activator claims the Summit points on an expedition basis
+
+Rules for Chasers
+
+With effect from 01-Jan-2004, only one QSO with a given Summit on any one day (defined as 00:00 to 23:59 UTC) counts for points.
+
+ */
+
 const ACTIVITY = {
   key: 'sota',
   comingSoon: true,
