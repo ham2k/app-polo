5379a5f495a620f38f32334ec17d00f4382bd0da
Sebastian Delmont <sd@notso.net>
2024-03-07 06:36:17 -0500

    Secondary Exchange controls gain focus when activated

--- src/screens/OperationScreen/OpLoggingTab/components/LoggingPanel/SecondaryExchangePanel/TimeControl.jsx
+++ src/screens/OperationScreen/OpLoggingTab/components/LoggingPanel/SecondaryExchangePanel/TimeControl.jsx
@@ -1,22 +1,20 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import { View } from 'react-native'
 
 import TimeChip from '../../../../components/TimeChip'
 import { TimeInput } from '../../../../../components/TimeInput'
 import { DateInput } from '../../../../../components/DateInput'
 
-export const timeControl = {
-  key: 'time',
-  icon: 'clock-outline',
-  order: 0,
-  labelComponent: ({ qso, operation, settings, icon, style, styles, themeColor, selected, onChange }) => (
-    <TimeChip time={qso?.startOnMillis} icon={icon} style={style} styles={styles} themeColor={themeColor}
-      selected={selected} onChange={onChange}
-    />
-  ),
-  inputComponent: ({ qso, operation, settings, disabled, icon, style, styles, themeColor, handleFieldChange, handleSubmit, focusedRef }) => (
+const TimeControlInputs = ({ qso, operation, settings, disabled, icon, style, styles, themeColor, handleFieldChange, handleSubmit, focusedRef }) => {
+  const ref = useRef()
+  useEffect(() => {
+    ref?.current?.focus()
+  }, [])
+
+  return (
     <View style={{ flexDirection: 'row', paddingHorizontal: 0, gap: styles.oneSpace }}>
       <TimeInput
+        innerRef={ref}
         themeColor={themeColor}
         style={{ minWidth: styles.oneSpace * 11 }}
         valueInMillis={qso?.startOnMillis}
@@ -40,4 +38,16 @@ export const timeControl = {
       />
     </View>
   )
+}
+
+export const timeControl = {
+  key: 'time',
+  icon: 'clock-outline',
+  order: 0,
+  labelComponent: ({ qso, operation, settings, icon, style, styles, themeColor, selected, onChange }) => (
+    <TimeChip time={qso?.startOnMillis} icon={icon} style={style} styles={styles} themeColor={themeColor}
+      selected={selected} onChange={onChange}
+    />
+  ),
+  inputComponent: TimeControlInputs
 }
