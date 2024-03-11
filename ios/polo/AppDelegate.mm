b665bda243f67bff11b822088babadeb97555b85
Sebastian Delmont <sd@notso.net>
2024-03-11 06:44:40 -0400

    CodePush for iOS

--- ios/polo/AppDelegate.mm
+++ ios/polo/AppDelegate.mm
@@ -3,6 +3,7 @@
 #import <React/RCTBundleURLProvider.h>
 
 #import "RNSplashScreen.h"
+#import <CodePush/CodePush.h>
 
 @implementation AppDelegate
 
@@ -26,7 +27,7 @@
 #if DEBUG
   return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index"];
 #else
-  return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+  return [CodePush bundleURL];
 #endif
 }
 
