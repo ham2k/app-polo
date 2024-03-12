7951ede97730983e678bd8fb8cd923193a9d8b8a
Sebastian Delmont <sd@notso.net>
2024-03-11 22:58:03 -0400

    Improved time and frequency formats

--- src/screens/OperationScreens/OpLoggingTab/components/QSOItem.jsx
+++ src/screens/OperationScreens/OpLoggingTab/components/QSOItem.jsx
@@ -1,14 +1,14 @@
 import React, { useMemo } from 'react'
 import { Icon, Text, TouchableRipple } from 'react-native-paper'
 
-import { fmtShortTimeZulu } from '../../../../tools/timeFormats'
+import { fmtDateTimeZuluDynamic } from '../../../../tools/timeFormats'
 import { View } from 'react-native'
 
 import { activityIndex } from '../../activities'
 import { DXCC_BY_PREFIX } from '@ham2k/lib-dxcc-data'
 import { annotateFromCountryFile } from '@ham2k/lib-country-files'
 import { parseCallsign } from '@ham2k/lib-callsigns'
-import { fmtFreqInMHz } from '../../../../tools/frequencyFormats'
+import { partsForFreqInMHz } from '../../../../tools/frequencyFormats'
 
 export function guessItemHeight (qso, styles) {
   return styles.compactRow.height + styles.compactRow.borderBottomWidth
@@ -29,12 +29,26 @@ const QSOItem = React.memo(function QSOItem ({ qso, ourInfo, onPress, styles, se
     }
   }, [qso?.their])
 
+  const freqParts = useMemo(() => {
+    if (qso.freq) {
+      return partsForFreqInMHz(qso.freq)
+    } else {
+      return [null, qso.band, null]
+    }
+  }, [qso])
+
   return (
     <TouchableRipple onPress={() => onPress && onPress({ qso })} style={{ backgroundColor: selected ? styles.theme.colors.secondaryLight : undefined }}>
       <View style={styles.compactRow}>
         <Text style={styles.fields.number}>{qso._number}</Text>
-        <Text style={styles.fields.time}>{fmtShortTimeZulu(qso.startOnMillis)}</Text>
-        <Text style={styles.fields.freq}>{qso.freq ? fmtFreqInMHz(qso.freq, { mode: 'compact' }) : qso.band}</Text>
+        <Text style={styles.fields.time}>{fmtDateTimeZuluDynamic(qso.startOnMillis, { compact: !extendedWidth })}</Text>
+        <Text style={styles.fields.freq}>
+          {freqParts[0] && <Text style={styles.fields.freqMHz}>{freqParts[0]}.</Text>}
+          {freqParts[1] && <Text style={styles.fields.freqKHz}>{freqParts[1]}</Text>}
+          {freqParts[2] && <Text style={styles.fields.freqHz}>
+            {extendedWidth ? `.${freqParts[2]}` : `.${freqParts[2].substring(0, 1)}`}
+          </Text>}
+        </Text>
         <Text style={styles.fields.call}>
           {qso.their?.call ?? '?'}
           {theirInfo?.entityPrefix && (settings.dxFlags === 'all' || (settings.dxFlags !== 'none' && theirInfo.entityPrefix !== ourInfo?.entityPrefix)) && (
