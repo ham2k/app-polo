f1134dd168a26b7cbe67aae171c0cd888c42204a
Sebastian Delmont <sd@notso.net>
2024-03-25 06:43:11 -0400

    Much better time and date inputs

--- src/screens/components/TimeInput.jsx
+++ src/screens/components/TimeInput.jsx
@@ -1,13 +1,16 @@
-import React, { useCallback, useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useRef, useState } from 'react'
 import ThemedTextInput from './ThemedTextInput'
 import { fmtTimeZulu } from '../../tools/timeFormats'
 import { useSelector } from 'react-redux'
 import { selectSecondsTick } from '../../store/time'
 
-const VALID_TIME_REGEX = /^(\d{1,2}|\d{1,2}:\d{0,2}|\d{1,2}:\d{1,2}:\d{0,2})$/
+const VALID_TIME_REGEX = /^(\d{1,2}|\d{1,2}:\d{2}|\d{1,2}:\d{2}:\d{2})$/
 
 export function TimeInput (props) {
-  const { label, valueInMillis, onChange, onBlur, fieldId } = props
+  const { label, valueInMillis, onChange, onBlur, fieldId, innerRef } = props
+
+  const localRef = useRef()
+  const actualInnerRef = innerRef ?? localRef
 
   const now = useSelector(selectSecondsTick)
 
@@ -18,38 +21,60 @@ export function TimeInput (props) {
   }, [now])
 
   const [localValue, setLocalValue] = useState()
+  const [originalValue, setOriginalValue] = useState()
 
   useEffect(() => {
+    if (actualInnerRef?.current?.isFocused()) return
+
     setLocalValue(fmtTimeZulu(valueInMillis, { showZ: false }))
-  }, [valueInMillis])
+    setOriginalValue(valueInMillis)
+  }, [valueInMillis, actualInnerRef])
+
+  const handleBlur = useCallback((event) => {
+    setLocalValue(fmtTimeZulu(valueInMillis, { showZ: false }))
+    setOriginalValue(valueInMillis)
+    onBlur && onBlur(event)
+  }, [valueInMillis, onBlur])
 
   const handleChange = useCallback((event) => {
     const { text } = event.nativeEvent
-    if (text.match(VALID_TIME_REGEX)) {
-      setLocalValue(text)
-    }
-  }, [])
+    setLocalValue(text)
 
-  const handleBlur = useCallback((event) => {
-    if (localValue.match(/^\d{1,2}:\d{1,2}$/)) {
-      let baseDate
-      if (valueInMillis) {
-        baseDate = new Date(valueInMillis).toISOString().split('T')[0]
-      } else {
-        baseDate = new Date().toISOString().split('T')[0]
-      }
+    let fullBaseTime
+    let newValue
+    if (originalValue) {
+      fullBaseTime = new Date(originalValue).toISOString()
+    } else {
+      fullBaseTime = new Date().toISOString()
+    }
+    const baseDate = fullBaseTime.split('T')[0]
 
-      const date = Date.parse(`${baseDate}T${localValue}Z`)
-      if (date) {
-        onChange && onChange({ value: date.valueOf(), fieldId })
+    if (text?.match(VALID_TIME_REGEX)) {
+      newValue = Date.parse(`${baseDate}T${text}Z`)
+    } else {
+      const parts = text?.match(/^\d{2}:\d{2}:\d{2}([-+]\d{1,})([hms])$/)
+      if (parts) {
+        let delta = parseInt(parts[1], 10)
+        if (parts[2] === 'h') delta = delta * 1000 * 60 * 60
+        else if (parts[2] === 'm') delta = delta * 1000 * 60
+        else if (parts[2] === 's') delta = delta * 1000
+
+        newValue = Date.parse(fullBaseTime).valueOf() + delta
       }
     }
-    onBlur && onBlur(event)
-  }, [localValue, onChange, valueInMillis, fieldId, onBlur])
+
+    if (newValue) {
+      newValue = newValue.valueOf()
+      if (newValue !== valueInMillis) {
+        onChange && onChange({ value: newValue, fieldId })
+      }
+    }
+  }, [fieldId, onChange, originalValue, valueInMillis])
 
   return (
     <ThemedTextInput
       {...props}
+      innerRef={actualInnerRef}
       value={localValue}
       label={label ?? 'Time'}
       placeholder={nowStr}
