cde3b07d346e44ab8a79e7c46b3a07cb5079b21c
Sebastian Delmont <sd@notso.net>
2024-03-28 18:40:07 -0400

    Camelcase null strings

--- src/tools/stringTools.js
+++ src/tools/stringTools.js
@@ -1,7 +1,7 @@
 import { capitalizeString } from './capitalizeString'
 
 export function camelCaseToWords (camelCase, { capitalize } = {}) {
-  let words = camelCase.replace(/([A-Z])/g, ' $1')
+  let words = (camelCase || '').replace(/([A-Z])/g, ' $1')
   if (capitalize) words = capitalizeString(words)
 
   return words
