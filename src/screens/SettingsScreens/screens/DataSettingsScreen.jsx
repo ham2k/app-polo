47aa6d5ef774c0a7c8f0cbae23a558690e6ac8ea
Sebastian Delmont <sd@notso.net>
2024-04-11 19:58:13 -0400

    Import ADIF into historical data

--- src/screens/SettingsScreens/screens/DataSettingsScreen.jsx
+++ src/screens/SettingsScreens/screens/DataSettingsScreen.jsx
@@ -1,7 +1,9 @@
-import React, { useCallback, useMemo, useState } from 'react'
+/* eslint-disable react/no-unstable-nested-components */
+import React, { useCallback, useEffect, useMemo, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Button, Dialog, List, Portal, Text } from 'react-native-paper'
 import { KeyboardAvoidingView, ScrollView } from 'react-native'
+import DocumentPicker from 'react-native-document-picker'
 
 import ScreenContainer from '../../components/ScreenContainer'
 import { useThemedStyles } from '../../../styles/tools/useThemedStyles'
@@ -10,6 +12,9 @@ import { fmtDateTimeNice, fmtDateTimeRelative } from '../../../tools/timeFormats
 import { fetchDataFile } from '../../../store/dataFiles/actions/dataFileFS'
 import { selectSettings } from '../../../store/settings'
 import { findHooks } from '../../../extensions/registry'
+import { countHistoricalRecords, importHistoricalADIF } from '../../../store/operations'
+import { countTemplate } from '../../../tools/stringTools'
+import { fmtNumber } from '@ham2k/lib-format-tools'
 
 const DataFileDefinitionItem = ({ def, settings, info, styles, onPress }) => {
   const Icon = useMemo(() => (
@@ -67,6 +72,7 @@ const DataFileDefinitionDialog = ({ def, info, settings, styles, onDialogDone })
 export default function DataSettingsScreen ({ navigation }) {
   const styles = useThemedStyles()
 
+  const dispatch = useDispatch()
   const settings = useSelector(selectSettings)
 
   const dataFileDefinitions = useMemo(() => getDataFileDefinitions(), [])
@@ -83,8 +89,46 @@ export default function DataSettingsScreen ({ navigation }) {
     return hooks
   }, [])
 
+  const [loadingHistoricalMessage, setLoadingHistoricalMessage] = useState()
+  const [historicalCount, setHistoricalCount] = useState()
+  useEffect(() => {
+    setTimeout(async () => {
+      const count = await dispatch(countHistoricalRecords())
+      setHistoricalCount(count)
+    }, 0)
+  }, [dispatch])
+
+  const handleImportHistoricalFile = useCallback(() => {
+    DocumentPicker.pickSingle().then(async (file) => {
+      setLoadingHistoricalMessage('Importing ADIF records... Please be patient!')
+      const interval = setInterval(async () => {
+        const count = await dispatch(countHistoricalRecords())
+        setHistoricalCount(count)
+      }, 1000)
+      await dispatch(importHistoricalADIF(file.uri))
+
+      clearInterval(interval)
+
+      setLoadingHistoricalMessage()
+      const count = await dispatch(countHistoricalRecords())
+      setHistoricalCount(count)
+    })
+  }, [dispatch])
+
   return (
     <ScreenContainer>
+      {loadingHistoricalMessage && (
+        <Portal>
+          <KeyboardAvoidingView style={{ flex: 1 }} behavior={'height'}>
+            <Dialog visible={true}>
+              <Dialog.Content>
+                <Text variant="bodyMedium" style={{ textAlign: 'center' }}>{loadingHistoricalMessage}</Text>
+              </Dialog.Content>
+            </Dialog>
+          </KeyboardAvoidingView>
+        </Portal>
+      )}
+
       <ScrollView style={{ flex: 1 }}>
         <List.Section>
           <List.Subheader>Offline Data</List.Subheader>
@@ -103,6 +147,16 @@ export default function DataSettingsScreen ({ navigation }) {
               )}
             </React.Fragment>
           ))}
+        </List.Section>
+
+        <List.Section>
+          <List.Subheader>Log Data</List.Subheader>
+          <List.Item
+            title="Import History from ADIF"
+            description={historicalCount && countTemplate(historicalCount, { zero: 'No records', one: '1 record', more: '{fmtCount} records' }, { fmtCount: fmtNumber(historicalCount) })}
+            left={() => <List.Icon style={{ marginLeft: styles.oneSpace * 2 }} icon="database-import-outline" />}
+            onPress={handleImportHistoricalFile}
+          />
         </List.Section>
 
         {extensionSettingHooks.length > 0 && (
