a77d0e3a8928c8ea875f7f61521bbd59bd6a163e
Sebastian Delmont <sd@notso.net>
2024-04-18 17:16:19 -0400

    Font sizes

--- src/styles/tools/computeSizes.js
+++ src/styles/tools/computeSizes.js
@@ -1,29 +1,36 @@
+/*
+ * Copyright ©️ 2024 Sebastian Delmont <sd@ham2k.com>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+ * If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
 import { PixelRatio, Dimensions } from 'react-native'
 
 export function computeSizes () {
   const { width, height } = Dimensions.get('window')
   const fontScale = PixelRatio.getFontScale()
 
-  let size
-  if (width / fontScale < 340) size = 'xs'
-  else if (width / fontScale < 400) size = 'sm'
-  else if (width / fontScale < 1000) size = 'md'
-  else if (width / fontScale < 1200) size = 'lg'
-  else size = 'xl'
-
-  const portrait = height > width
-  const landscape = !portrait
-
   // If the screen is too small, and the font scale too large, nothing will fit, so we need to adjust our font sizes down
   let fontScaleAdjustment = 1
-  if (size === 'xs') {
+  if (width < 340) {
     fontScaleAdjustment = width / fontScale / 330
   }
 
   // For Tablets, lets bump the font size a bit
-  if (size === 'md' || size === 'lg' || size === 'xl') {
-    fontScaleAdjustment = fontScaleAdjustment * 1.1
+  if (width > 500) {
+    fontScaleAdjustment = fontScaleAdjustment * 1.07
   }
+
+  let size
+  if (width / (fontScale * fontScaleAdjustment) < 340) size = 'xs'
+  else if (width / (fontScale * fontScaleAdjustment) < 400) size = 'sm'
+  else if (width / (fontScale * fontScaleAdjustment) < 1000) size = 'md'
+  else if (width / (fontScale * fontScaleAdjustment) < 1200) size = 'lg'
+  else size = 'xl'
+
+  const portrait = height > width
+  const landscape = !portrait
 
   return {
     size,
