8c266a1572cef48c2bcc0c6ee7500906c5974a05
Sebastian Delmont <sd@notso.net>
2024-04-19 17:27:22 -0400

    [DIST] update dialog component

--- src/screens/SettingsScreens/components/UpdateTracksDialog.jsx
+++ src/screens/SettingsScreens/components/UpdateTracksDialog.jsx
@@ -1,8 +1,10 @@
 import React, { useCallback, useEffect, useState } from 'react'
-import { Button, Dialog, Portal, RadioButton, Text } from 'react-native-paper'
+import { Button, Dialog, RadioButton, Text } from 'react-native-paper'
 import { useDispatch } from 'react-redux'
+import { View } from 'react-native'
+
 import { setSettings } from '../../../store/settings'
-import { KeyboardAvoidingView, View } from 'react-native'
+import { Ham2kDialog } from '../../components/Ham2kDialog'
 import { UPDATE_TRACK_LABELS } from '../screens/VersionSettingsScreen'
 
 export function UpdateTracksDialog ({ visible, settings, styles, onDialogDone, dismissable }) {
@@ -32,36 +34,32 @@ export function UpdateTracksDialog ({ visible, settings, styles, onDialogDone, d
   }, [settings, onDialogDone])
 
   return (
-    <Portal>
-      <KeyboardAvoidingView style={{ flex: 1 }} behavior={'height'}>
-        <Dialog visible={dialogVisible} onDismiss={handleCancel} dismissable={dismissable ?? true}>
-          <Dialog.Icon icon={'glass-fragile'} />
-          <Dialog.Title style={{ textAlign: 'center' }}>Update Track</Dialog.Title>
-          <Dialog.Content>
-            <RadioButton.Group
-              onValueChange={(v) => setValue(v)}
-              value={value}
-            >
-              <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                <RadioButton value="Production" />
-                <Text onPress={() => setValue('Production')} style={styles.rowText}>{UPDATE_TRACK_LABELS.Production}</Text>
-              </View>
-              <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                <RadioButton value="Staging" />
-                <Text onPress={() => setValue('Staging')} style={styles.rowText}>{UPDATE_TRACK_LABELS.Staging}</Text>
-              </View>
-              <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                <RadioButton value="Development" />
-                <Text onPress={() => setValue('Development')} style={styles.rowText}>{UPDATE_TRACK_LABELS.Development}</Text>
-              </View>
-            </RadioButton.Group>
-          </Dialog.Content>
-          <Dialog.Actions>
-            <Button onPress={handleCancel}>Cancel</Button>
-            <Button onPress={handleAccept}>Ok</Button>
-          </Dialog.Actions>
-        </Dialog>
-      </KeyboardAvoidingView>
-    </Portal>
+    <Ham2kDialog visible={dialogVisible} onDismiss={handleCancel} dismissable={dismissable ?? true}>
+      <Dialog.Icon icon={'glass-fragile'} />
+      <Dialog.Title style={{ textAlign: 'center' }}>Update Track</Dialog.Title>
+      <Dialog.Content>
+        <RadioButton.Group
+          onValueChange={(v) => setValue(v)}
+          value={value}
+        >
+          <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+            <RadioButton value="Production" />
+            <Text onPress={() => setValue('Production')} style={styles.rowText}>{UPDATE_TRACK_LABELS.Production}</Text>
+          </View>
+          <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+            <RadioButton value="Staging" />
+            <Text onPress={() => setValue('Staging')} style={styles.rowText}>{UPDATE_TRACK_LABELS.Staging}</Text>
+          </View>
+          <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+            <RadioButton value="Development" />
+            <Text onPress={() => setValue('Development')} style={styles.rowText}>{UPDATE_TRACK_LABELS.Development}</Text>
+          </View>
+        </RadioButton.Group>
+      </Dialog.Content>
+      <Dialog.Actions>
+        <Button onPress={handleCancel}>Cancel</Button>
+        <Button onPress={handleAccept}>Ok</Button>
+      </Dialog.Actions>
+    </Ham2kDialog>
   )
 }
