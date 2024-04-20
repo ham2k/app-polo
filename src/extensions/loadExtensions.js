5f7acf6cdcceb3caf61a399c6d7614cd7581dc74
Sebastian Delmont <sd@notso.net>
2024-04-19 20:51:43 -0400

    Removed SEQP

--- src/extensions/loadExtensions.js
+++ src/extensions/loadExtensions.js
@@ -14,7 +14,6 @@ import SOTAExtension from './activities/sota/SOTAExtension'
 import WWFFExtension from './activities/wwff/WWFFExtension'
 import FDExtension from './activities/fd/FDExtension'
 import WFDExtension from './activities/wfd/WFDExtension'
-import SEQPExtension from './activities/seqp/SEQPExtension'
 import CustomExtension from './activities/custom/CustomExtension'
 
 import RadioCommands from './commands/RadioCommands'
@@ -33,7 +32,6 @@ const loadExtensions = () => async (dispatch, getState) => {
   registerExtension(CustomExtension)
   registerExtension(WFDExtension)
   registerExtension(FDExtension)
-  registerExtension(SEQPExtension)
 
   registerExtension(RadioCommands)
   registerExtension(TimeCommands)
