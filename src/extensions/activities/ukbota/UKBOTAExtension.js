c3551dd2519a2856ccaaa5e57a9d5eb8399d30e2
Steven Hiscocks <steven@hiscocks.me.uk>
2024-04-28 08:37:06 +0100

    Add UKBOTA extension
    Includes fetching public bunker list from official source (Google
    Drive); prefix guessing based on callsign; automatically adding
    slashes/dashes when inputting bunker to bunker.
    
    There is no specific requirement for log uploading, so for simplicity of
    output, multiple B2Bs are comma seperated in SIG_INFO.

---
+++ src/extensions/activities/ukbota/UKBOTAExtension.js
@@ -0,0 +1,163 @@
+/*
+ * Copyright ©️ 2024 Sebastian Delmont <sd@ham2k.com>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+ * If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+import { loadDataFile, removeDataFile } from '../../../store/dataFiles/actions/dataFileFS'
+import { filterRefs, findRef, refsToString } from '../../../tools/refTools'
+
+import { Info } from './UKBOTAInfo'
+import { UKBOTAActivityOptions } from './UKBOTAActivityOptions'
+import { ukbotaFindOneByReference, registerUKBOTADataFile } from './UKBOTADataFile'
+import { UKBOTALoggingControl } from './UKBOTALoggingControl'
+
+const Extension = {
+  ...Info,
+  category: 'locationBased',
+  onActivationDispatch: ({ registerHook }) => async (dispatch) => {
+    registerHook('activity', { hook: ActivityHook })
+    registerHook(`ref:${Info.huntingType}`, { hook: ReferenceHandler })
+    registerHook(`ref:${Info.activationType}`, { hook: ReferenceHandler })
+
+    registerUKBOTADataFile()
+    await dispatch(loadDataFile('ukbota-all-bunkers', { noticesInsteadOfFetch: true }))
+  },
+  onDeactivationDispatch: () => async (dispatch) => {
+    await dispatch(removeDataFile('ukbota-all-bunkers'))
+  }
+}
+export default Extension
+
+const ActivityHook = {
+  ...Info,
+  MainExchangePanel: null,
+  loggingControls: ({ operation, settings }) => {
+    if (findRef(operation, Info.activationType)) {
+      return [ActivatorLoggingControl]
+    } else {
+      return [HunterLoggingControl]
+    }
+  },
+  Options: UKBOTAActivityOptions,
+
+  includeControlForQSO: ({ qso, operation }) => {
+    if (findRef(operation, Info.activationType)) return true
+    if (findRef(qso, Info.huntingType)) return true
+    else return false
+  },
+
+  labelControlForQSO: ({ operation, qso }) => {
+    const opRef = findRef(operation, Info.activationType)
+    let label = opRef ? Info.shortNameDoubleContact : Info.shortName
+    if (findRef(qso, Info.huntingType)) label = `✓ ${label}`
+    return label
+  }
+}
+
+const HunterLoggingControl = {
+  key: `${Info.key}/hunter`,
+  order: 10,
+  icon: Info.icon,
+  label: ({ operation, qso }) => {
+    const parts = [Info.shortName]
+    if (findRef(qso, Info.huntingType)) parts.unshift('✓')
+    return parts.join(' ')
+  },
+  InputComponent: UKBOTALoggingControl,
+  optionType: 'optional'
+}
+
+const ActivatorLoggingControl = {
+  key: `${Info.key}/activator`,
+  order: 10,
+  icon: Info.icon,
+  label: ({ operation, qso }) => {
+    const parts = [Info.shortNameDoubleContact]
+    if (findRef(qso, Info.huntingType)) parts.unshift('✓')
+    return parts.join(' ')
+  },
+  InputComponent: UKBOTALoggingControl,
+  optionType: 'mandatory'
+}
+
+const ReferenceHandler = {
+  ...Info,
+
+  shortDescription: (operation) => refsToString(operation, Info.activationType),
+
+  description: (operation) => {
+    const refs = filterRefs(operation, Info.activationType)
+    return [
+      refs.map(r => r.ref).filter(x => x).join(', '),
+      refs.map(r => r.name).filter(x => x).join(', ')
+    ].filter(x => x).join(' • ')
+  },
+
+  incQsoItemIcon: true,
+
+  decorateRefWithDispatch: (ref) => async () => {
+    if (!ref?.ref || !ref.ref.match(Info.referenceRegex)) return { ...ref, ref: '', name: '', location: '' }
+
+    const data = await ukbotaFindOneByReference(ref.ref)
+    let result
+    if (data?.name) {
+      result = {
+        ...ref,
+        name: data.name,
+        location: data.area,
+        grid: data.grid
+      }
+    } else {
+      return { ...ref, name: Info.unknownReferenceName ?? 'Unknown reference' }
+    }
+    return result
+  },
+
+  suggestOperationTitle: (ref) => {
+    if (ref.type === Info.activationType && ref.ref) {
+      return { at: ref.ref, subtitle: ref.name }
+    } else {
+      return null
+    }
+  },
+
+  suggestExportOptions: ({ operation, ref, settings }) => {
+    if (ref.type === Info.activationType && ref.ref) {
+      return [{
+        format: 'adif',
+        common: { refs: [ref] },
+        nameTemplate: settings.useCompactFileNames ? '{call}@{ref}-{compactDate}' : '{date} {call} at {ref}',
+        titleTemplate: `{call}: ${Info.shortName} at ${[ref.ref, ref.name].filter(x => x).join(' - ')} on {date}`
+      }]
+    }
+  },
+
+  adifFieldsForOneQSO: ({ qso, operation, common }) => {
+    const huntingRefs = filterRefs(qso, Info.huntingType)
+
+    if (huntingRefs) return ([{ SIG: 'UKBOTA' }, { SIG_INFO: huntingRefs.map(ref => ref.ref).filter(x => x).join(',') }])
+    else return []
+  },
+
+  adifFieldCombinationsForOneQSO: ({ qso, operation, common }) => {
+    const huntingRefs = filterRefs(qso, Info.huntingType)
+    const activationRef = findRef(operation, Info.activationType)
+    let activationADIF = []
+    if (activationRef) {
+      activationADIF = [
+        { MY_SIG: 'UKBOTA' }, { MY_SIG_INFO: activationRef.ref }
+      ]
+    }
+
+    if (huntingRefs.length > 0) {
+      return [[
+        ...activationADIF,
+        { SIG: 'UKBOTA' }, { SIG_INFO: huntingRefs.map(ref => ref.ref).filter(x => x).join(',') }
+      ]]
+    } else {
+      return [activationADIF]
+    }
+  }
+}
