b4f78daff6fd1e5d306ead0394641c3cd44b917d
Sebastian Delmont <sd@notso.net>
2024-04-29 10:51:12 -0400

    Fix table creation and migration

--- src/store/db/createTables.js
+++ src/store/db/createTables.js
@@ -13,13 +13,37 @@ export async function createTables (db) {
     const row = await dbSelectOne('SELECT version FROM version LIMIT 1', [], { db })
     version = row?.version ?? 0
   } catch (e) {
-    console.log('createTables -- error getting version', e)
     version = 0
   }
 
-  if (version === 2) {
-    // console.log('createTables -- using version 1')
-  } else if (version === 1) {
+  if (version < 1) {
+    console.log('createTables -- creating version 1')
+    await dbExecute(`
+      CREATE TABLE IF NOT EXISTS version (
+        version INTEGER PRIMARY KEY NOT NULL
+      )`, [], { db })
+    await dbExecute(`
+      CREATE TABLE IF NOT EXISTS operations (
+        uuid TEXT PRIMARY KEY NOT NULL,
+        data TEXT
+      )`, [], { db })
+    await dbExecute(`
+      CREATE TABLE IF NOT EXISTS qsos (
+        key TEXT NOT NULL,
+        operation TEXT NOT NULL,
+        ourCall TEXT,
+        theirCall TEXT
+        mode TEXT,
+        band TEXT,
+        startOnMillis INTEGER,
+        data TEXT,
+        PRIMARY KEY (key, operation)
+      )`, [], { db })
+    await dbExecute('INSERT INTO version (version) VALUES (?)', [1], { db })
+  }
+
+  if (version < 2) {
+    console.log('createTables -- creating version 2')
     await dbExecute(`
       CREATE TABLE IF NOT EXISTS lookups (
         category TEXT NOT NULL,
@@ -35,30 +59,9 @@ export async function createTables (db) {
       )`, [], { db })
 
     await dbExecute('UPDATE version SET version = 2', [], { db })
-  } else if (version === 0) {
-    // console.log('createTables -- creating version 1')
-    await dbExecute(`
-      CREATE TABLE IF NOT EXISTS version (
-        version INTEGER PRIMARY KEY NOT NULL
-      )`, [], { db })
-    await dbExecute(`
-      CREATE TABLE IF NOT EXISTS operations (
-        uuid TEXT PRIMARY KEY NOT NULL,
-        data TEXT
-      )`, [], { db })
-    await dbExecute(`
-      CREATE TABLE IF NOT EXISTS qsos (
-        key TEXT
-        operation TEXT,
-        ourCall TEXT,
-        theirCall TEXT
-        mode TEXT,
-        band TEXT,
-        startOnMillis INTEGER,
-        data TEXT,
-        PRIMARY KEY (key, operation)
-      )`, [], { db })
-    await dbExecute('DELETE FROM version', [], { db })
-    await dbExecute('INSERT INTO version (version) VALUES (?)', [1], { db })
+  }
+
+  if (version === 2) {
+    // console.log('createTables -- using version 1')
   }
 }
