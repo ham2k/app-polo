ac30ee9aaee2d97d8caa1747e66316863cbe7974
Sebastian Delmont <sd@notso.net>
2024-04-29 09:18:38 -0400

    Easy override for markdown body style

--- src/screens/components/Ham2kMarkdown.jsx
+++ src/screens/components/Ham2kMarkdown.jsx
@@ -5,17 +5,20 @@
  * If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
-import React from 'react'
+import React, { useMemo } from 'react'
 
 import Markdown from 'react-native-markdown-display'
 
 import { useThemedStyles } from '../../styles/tools/useThemedStyles'
 
-export function Ham2kMarkdown ({ children, styles }) {
+export function Ham2kMarkdown ({ children, styles, style }) {
   const defaultStyles = useThemedStyles()
-  const style = styles?.markdown ?? defaultStyles?.markdown
+  const markdownStyle = useMemo(() => {
+    const combinedStyle = { ...defaultStyles?.markdown, ...styles?.markdown }
+    if (style) combinedStyle.body = { ...combinedStyle?.body, ...style }
+    return combinedStyle
+  }, [styles, style, defaultStyles])
 
   if (children.join) children = children.join('')
-
-  if (children) return <Markdown style={style}>{children}</Markdown>
+  if (children) return <Markdown style={markdownStyle}>{children}</Markdown>
 }
