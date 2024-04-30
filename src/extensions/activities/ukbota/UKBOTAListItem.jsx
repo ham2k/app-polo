38115de0983b5f54a4ceb8f28f266fc7e003ea93
Steven Hiscocks <steven@hiscocks.me.uk>
2024-04-30 12:40:11 +0100

    Add UKBOTA extension (#13)
    * Add extension ref handler option for P2P icons
    
    * Add UKBOTA extension
    
    Includes fetching public bunker list from official source (Google
    Drive); prefix guessing based on callsign; automatically adding
    slashes/dashes when inputting bunker to bunker.
    
    There is no specific requirement for log uploading, so for simplicity of
    output, multiple B2Bs are comma seperated in SIG_INFO.
    
    * Add hook for ADIF comment header for UKBOTA
    
    This is useful in case of UKBOTA, and possibly other extensions in
    future, where log submission is manual.

---
+++ src/extensions/activities/ukbota/UKBOTAListItem.jsx
@@ -0,0 +1,53 @@
+/*
+ * Copyright ©️ 2024 Sebastian Delmont <sd@ham2k.com>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+ * If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+/* eslint-disable react/no-unstable-nested-components */
+import React, { useEffect, useMemo, useState } from 'react'
+import { IconButton, List, Text } from 'react-native-paper'
+import { View } from 'react-native'
+
+import { fmtDistance } from '../../../tools/geoTools'
+
+import { Info } from './UKBOTAInfo'
+import { ukbotaFindOneByReference } from './UKBOTADataFile'
+import { Ham2kListItem } from '../../../screens/components/Ham2kListItem'
+
+export function UKBOTAListItem ({ activityRef, refData, allRefs, style, styles, settings, onPress, onAddReference, onRemoveReference, online }) {
+  const [reference, setReference] = useState()
+  useEffect(() => {
+    ukbotaFindOneByReference(activityRef).then(setReference)
+  }, [activityRef])
+
+  const isInRefs = useMemo(() => {
+    return allRefs.find(ref => ref.ref === activityRef)
+  }, [allRefs, activityRef])
+
+  return (
+    <Ham2kListItem style={{ paddingRight: styles.oneSpace * 1 }}
+      title={
+        <View style={{ flexDirection: 'row', justifyContent: 'space-between', gap: styles.oneSpace }}>
+          <Text style={{ fontWeight: 'bold' }}>
+            {reference?.ref ?? activityRef}
+          </Text>
+          <Text>
+            {refData?.distance && fmtDistance(refData.distance, { units: settings.distanceUnits }) + ' away'}
+          </Text>
+        </View>
+      }
+      description={[reference?.name, reference?.area].filter(x => x).join(', ')}
+      onPress={onPress}
+      left={() => <List.Icon style={{ marginLeft: styles.oneSpace * 2 }} icon={Info.icon} />}
+      right={() => (
+        isInRefs ? (
+          onRemoveReference && <IconButton icon="minus-circle-outline" onPress={() => onRemoveReference(activityRef)} />
+        ) : (
+          onAddReference && <IconButton icon="plus-circle" onPress={() => onAddReference(activityRef)} />
+        )
+      )}
+    />
+  )
+}
