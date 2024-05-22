1f9b632d62a823a9478b21b957cae659b6cfa681
Sebastian Delmont <sd@notso.net>
2024-05-22 09:01:38 -0400

    Align history pill

--- src/screens/OperationScreens/OpLoggingTab/components/LoggingPanel/CallInfo.jsx
+++ src/screens/OperationScreens/OpLoggingTab/components/LoggingPanel/CallInfo.jsx
@@ -40,8 +40,10 @@ function prepareStyles (baseStyles, themeColor) {
     history: {
       pill: {
         marginRight: baseStyles.halfSpace,
+        lineHeight: baseStyles.normalFontSize * 1.3,
         borderRadius: 3,
-        padding: baseStyles.oneSpace * 0.3,
+        // padding: baseStyles.oneSpace * 0.3,
+        marginTop: baseStyles.oneSpace * 0.5,
         paddingHorizontal: baseStyles.oneSpace * 0.5,
         backgroundColor: baseStyles.theme.colors[`${themeColor}Light`]
       },
@@ -66,7 +68,12 @@ function prepareStyles (baseStyles, themeColor) {
     },
     markdown: {
       ...baseStyles.markdown,
-      paragraph: { margin: 0, marginTop: baseStyles.halfSpace, marginBottom: 0 }
+      paragraph: {
+        margin: 0,
+        marginTop: baseStyles.halfSpace,
+        marginBottom: 0,
+        lineHeight: baseStyles.normalFontSize * 1.3
+      }
     }
   }
 }
