b77709bd716cf182af5b4938917bfdcf7c991430
Steven Hiscocks <steven@hiscocks.me.uk>
2024-05-22 11:28:51 +0100

    Add Global Mountain Activity (#17)
    * Add Global Mountain Activity
    
    Similar to SOTA, with summit to summit activity, but more flexible
    reference and region in references, so less reference validation, and
    no ability to add `-` or `/` automatically in S2S input.
    
    GMA site does support `GMA_REF` and `SIG`/`SIG_INFO` with *GMA* as
    `SIG` for logs. As `GMA_REF` isn't in ADIF standard, opted for
    `SIG`/`SIG_INFO` as will ensure ADIF is valid for other applications.
    
    This activity was based on SOTA with modifications due to similarity.
    
    * Add GMA spotting
    
    * Add GMA to release notes
    
    * Add common method for GMA and WWFF spotting
    
    GMA spotting can also be extended to other activities in future
    
    ---------
    
    Co-authored-by: Sebastian Delmont <sd@notso.net>

---
+++ src/extensions/activities/GMACommonPostSpot.js
@@ -0,0 +1,56 @@
+/*
+ * Copyright ©️ 2024 Sebastian Delmont <sd@ham2k.com>
+ * Copyright ©️ 2024 Steven Hiscocks <steven@hiscocks.me.uk>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+ * If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+import packageJson from '../../../package.json'
+import { parseCallsign } from '@ham2k/lib-callsigns'
+import { ADIF_SUBMODES } from '@ham2k/lib-operation-data'
+
+const validModes = ['CW', 'FM', 'SSB', 'RTTY', 'PSK']
+
+export const GMACommonPostSpot = (operation, comments, refs, url) => async (dispatch, getState) => {
+  const state = getState()
+  const call = operation.stationCall || state.settings.operatorCall
+  const baseCall = parseCallsign(call).baseCall
+
+  let mode = operation.mode
+  if (!validModes.includes(operation.mode)) {
+    if (ADIF_SUBMODES.SSB.includes(mode)) mode = 'SSB'
+    else if (ADIF_SUBMODES.PSK.includes(mode)) mode = 'PSK'
+    else mode = 'other'
+  }
+
+  const mainRef = refs[0].ref
+  const refComment = refs.length > 1 ? `also ${refs.map((x) => (x.ref)).join(' ')}` : ''
+  try {
+    const response = await fetch(url ?? 'https://www.cqgma.org/spotsmart2.php', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'User-Agent': `Ham2K Portable Logger/${packageJson.version}`
+      },
+      body: new URLSearchParams({
+        yspotter: baseCall ?? call, // Spotter shouldn't include /P, etc.
+        ycall: call,
+        yreference: mainRef,
+        yqrg: operation.freq,
+        ymode: mode,
+        ycomment: [comments, refComment].filter((x) => (x)).join(' '),
+        B1: 'Submit'
+      }).toString()
+    })
+    if (response.status === 200) {
+      // const body = await response.text()
+      // console.log(body)
+    } else {
+      const body = await response.text()
+      console.error('Error in GMA Spot', { response, body })
+    }
+  } catch (error) {
+    console.error('Error in GMA Spot', error)
+  }
+}
---
+++ src/extensions/activities/gma/GMAInfo.js
@@ -0,0 +1,21 @@
+/*
+ * Copyright ©️ 2024 Sebastian Delmont <sd@ham2k.com>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+ * If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+export const Info = {
+  key: 'gma',
+  icon: 'summit',
+  name: 'Global Mountain Activity',
+  shortName: 'GMA',
+  doubleContact: 'Summit-to-Summit',
+  shortNameDoubleContact: 'S2S',
+  infoURL: 'https://www.cqgma.org/',
+  huntingType: 'gma',
+  activationType: 'gmaActivation',
+  descriptionPlaceholder: 'Enter GMA reference',
+  unknownReferenceName: 'Unknown Summit',
+  referenceRegex: /^(?:[A-Z0-9]+\/[A-Z]{2,}-[0-9]{3,}|UNM-(?:SH)?[0-9]{3})$/i
+}
