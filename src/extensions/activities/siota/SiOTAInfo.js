24554fb7f32db21b6e65c691eaecefe10acd5b74
Steven Hiscocks <steven@hiscocks.me.uk>
2024-05-24 17:00:56 +0100

    Add Silos on the Air (SiOTA)

---
+++ src/extensions/activities/siota/SiOTAInfo.js
@@ -0,0 +1,21 @@
+/*
+ * Copyright ©️ 2024 Sebastian Delmont <sd@ham2k.com>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+ * If a copy of the MPL was not distributed with this file, You can obtain one at https://www.silosontheair.com/about.html.
+ */
+
+export const Info = {
+  key: 'siota',
+  icon: 'silo',
+  name: 'Silos on the Air',
+  shortName: 'SiOTA',
+  doubleContact: 'Silo-to-Silo',
+  shortNameDoubleContact: 'Si2Si',
+  infoURL: 'https://www.silosontheair.com/about.html',
+  huntingType: 'siota',
+  activationType: 'siotaActivation',
+  descriptionPlaceholder: 'Enter SiOTA references',
+  unknownReferenceName: 'Unknown Silo',
+  referenceRegex: /^VK-[A-Z]{3}[0-9]+$/i
+}
