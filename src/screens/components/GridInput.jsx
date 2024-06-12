b0f8def30a0b3fb080d4d660d6b53050f1ea4642
Sebastian Delmont <sd@notso.net>
2024-06-12 08:16:57 -0400

    Extract GridInput component

---
+++ src/screens/components/GridInput.jsx
@@ -0,0 +1,73 @@
+/*
+ * Copyright ©️ 2024 Sebastian Delmont <sd@ham2k.com>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+ * If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+import React, { useEffect, useCallback, useState, useRef } from 'react'
+
+import { useUIState } from '../../store/ui/useUIState'
+import ThemedTextInput from './ThemedTextInput'
+
+const VALID_MAIDENHEAD_REGEX = /^([A-R]{2}|[A-R]{2}[0-9]{2}|[A-R]{2}[0-9]{2}[a-x]{2}||[A-R]{2}[0-9]{2}[a-x]{2}[0-9]{2})$/
+const PARTIAL_MAIDENHEAD_REGEX = /^([A-R]{0,2}|[A-R]{2}[0-9]{0,2}|[A-R]{2}[0-9]{2}[a-x]{0,2}||[A-R]{2}[0-9]{2}[a-x]{2}[0-9]{0,2})$/
+
+export default function GridInput (props) {
+  const { value, onChange, onChangeText, fieldId, innerRef } = props
+
+  // eslint-disable-next-line no-unused-vars
+  let [mode, setMode] = useUIState('NumberKeys', 'mode', 'numbers')
+
+  useEffect(() => {
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    mode = 'numbers'
+    setMode('numbers')
+  }, [value, setMode])
+
+  const localRef = useRef()
+  const actualInnerRef = innerRef ?? localRef
+
+  const [localValue, setLocalValue] = useState(value ?? '')
+
+  useEffect(() => {
+    if (actualInnerRef?.current?.isFocused()) return
+
+    setLocalValue(value ?? '')
+  }, [value, actualInnerRef])
+
+  const [isValid, setIsValidValue] = useState()
+
+  useEffect(() => {
+    setIsValidValue(VALID_MAIDENHEAD_REGEX.test(localValue))
+  }, [localValue])
+
+  const handleChange = useCallback((event) => {
+    let { text } = event.nativeEvent
+
+    text = text.substring(0, 4).toUpperCase() + text.substring(4).toLowerCase()
+    if (PARTIAL_MAIDENHEAD_REGEX.test(text)) {
+      setLocalValue(text)
+    }
+
+    if (text !== value) {
+      onChangeText && onChangeText(text)
+      onChange && onChange({ nativeEvent: { text }, fieldId })
+    } else if (text === '') {
+      onChangeText && onChangeText(undefined)
+      onChange && onChange({ nativeEvent: { text: undefined }, fieldId })
+    }
+  }, [fieldId, onChange, onChangeText, value])
+
+  return (
+    <ThemedTextInput
+      {...props}
+      value={localValue ?? ' '}
+      keyboard="dumb"
+      noSpaces={true}
+      maxLength={8}
+      error={value && !isValid}
+      onChange={handleChange}
+    />
+  )
+}
