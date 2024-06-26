66f44c19294228a1d01197a0e1ab60bb03f63d7d
Sebastian Delmont <sd@notso.net>
2024-06-26 08:49:49 -0400

    Upgraded to RN 0.74
    According to https://react-native-community.github.io/upgrade-helper/?from=0.73.8&to=0.74.2

--- android/app/src/main/java/com/polo/MainApplication.kt
+++ android/app/src/main/java/com/polo/MainApplication.kt
@@ -38,7 +38,7 @@ class MainApplication : Application(), ReactApplication {
       }
 
   override val reactHost: ReactHost
-    get() = getDefaultReactHost(this.applicationContext, reactNativeHost)
+    get() = getDefaultReactHost(applicationContext, reactNativeHost)
 
   override fun onCreate() {
     super.onCreate()
@@ -52,6 +52,5 @@ class MainApplication : Application(), ReactApplication {
       // If you opted-in for the New Architecture, we load the native entry point for this app.
       load()
     }
-    ReactNativeFlipper.initializeFlipper(this, reactNativeHost.reactInstanceManager)
   }
 }
--- react-native.config.js
+++ react-native.config.js
@@ -1,11 +1,5 @@
 module.exports = {
   project: {
-    android: {
-      unstable_reactLegacyComponentNames: ['AIRGoogleMap', 'AIRMap', 'AIRMapMarker', 'PanoramaView']
-    },
-    ios: {
-      unstable_reactLegacyComponentNames: ['AIRGoogleMap', 'AIRMap', 'AIRMapMarker', 'PanoramaView']
-    }
   },
   assets: ['./assets/fonts']
 }
