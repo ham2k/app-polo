6ac7d64e197c6bb559ce64f6c77bfa111ac996e6
Sebastian Delmont <sd@notso.net>
2024-08-06 08:20:55 -0400

    Null leak in scoring when not in an operation

--- src/extensions/activities/sota/SOTAExtension.js
+++ src/extensions/activities/sota/SOTAExtension.js
@@ -212,7 +212,7 @@ const ReferenceHandler = {
     const theirRef = findRef(qso, Info.huntingType)
     const points = theirRef?.ref ? 1 : 0
 
-    const nearDupes = qsos.filter(q => !q.deleted && (startOnMillis ? q.startOnMillis < startOnMillis : true) && q.their.call === qso.their.call && q.key !== key)
+    const nearDupes = (qsos || []).filter(q => !q.deleted && (startOnMillis ? q.startOnMillis < startOnMillis : true) && q.their.call === qso.their.call && q.key !== key)
 
     if (nearDupes.length === 0) {
       return { counts: 1, points, type: Info.activationType }
