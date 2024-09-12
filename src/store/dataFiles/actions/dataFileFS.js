9b3b0ba5ee31acac2e3051a0a1ebf027c0e805b9
Sebastian Delmont <sd@notso.net>
2024-09-12 09:46:30 -0400

    URL fetching function with support for ETags

--- src/store/dataFiles/actions/dataFileFS.js
+++ src/store/dataFiles/actions/dataFileFS.js
@@ -5,11 +5,15 @@
  * If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
+import { Buffer } from 'buffer'
 import RNFetchBlob from 'react-native-blob-util'
+
 import { getDataFileDefinition, getDataFileDefinitions } from '../dataFilesRegistry'
 import { actions, selectDataFileInfo } from '../dataFilesSlice'
 import { addRuntimeMessage } from '../../runtime'
 import { reportError } from '../../../distro'
+
+import packageJson from '../../../../package.json'
 
 import { addNotice } from '../../system/systemSlice'
 
@@ -19,15 +23,17 @@ import { addNotice } from '../../system/systemSlice'
  *
  *
  */
-export const fetchDataFile = (key, options = {}) => async (dispatch) => {
+export const fetchDataFile = (key, options = {}) => async (dispatch, getState) => {
   const definition = getDataFileDefinition(key)
   if (!definition) throw new Error(`No data file definition found for ${key}`)
+
+  const info = selectDataFileInfo(getState(), key)
 
   try {
     await dispatch(actions.setDataFileInfo({ key, status: 'fetching' }))
     options.onStatus && await options.onStatus({ key, definition, status: 'fetching' })
 
-    const data = await definition.fetch({ key, definition, options })
+    const data = await definition.fetch({ key, definition, info, dispatch, getState, options })
 
     try { await RNFetchBlob.fs.mkdir(`${RNFetchBlob.fs.dirs.DocumentDir}/data/`) } catch (error) { /* ignore */ }
     await RNFetchBlob.fs.writeFile(`${RNFetchBlob.fs.dirs.DocumentDir}/data/${definition.key}.json`, JSON.stringify(data))
@@ -58,10 +64,15 @@ export const readDataFile = (key, options = {}) => async (dispatch) => {
     const body = await RNFetchBlob.fs.readFile(`${RNFetchBlob.fs.dirs.DocumentDir}/data/${definition.key}.json`)
     const data = JSON.parse(body)
 
-    const stat = await RNFetchBlob.fs.stat(`${RNFetchBlob.fs.dirs.DocumentDir}/data/${definition.key}.json`)
-    const lastModified = new Date(stat.lastModified)
+    let date
+    if (data.date) {
+      date = new Date(data.date)
+    } else {
+      const stat = await RNFetchBlob.fs.stat(`${RNFetchBlob.fs.dirs.DocumentDir}/data/${definition.key}.json`)
+      date = new Date(stat.lastModified)
+    }
 
-    dispatch(actions.setDataFileInfo({ key, data, status: 'loaded', date: lastModified ?? new Date() }))
+    dispatch(actions.setDataFileInfo({ key, data, status: 'loaded', date }))
 
     let loadedOk
     if (definition.onLoad) loadedOk = await definition.onLoad(data, options)
@@ -140,4 +151,44 @@ export const loadAllDataFiles = () => async (dispatch) => {
   for (const definition of definitions) {
     await dispatch(loadDataFile(definition.key))
   }
+}
+
+export async function fetchAndProcessURL ({ url, key, process, definition, info, options }) {
+  const headers = {
+    'User-Agent': `Ham2K Portable Logger/${packageJson.version}`
+  }
+  console.log('fetching', { url, info })
+  if (info?.data?.etag) {
+    console.log('using etag', info.data.etag)
+    headers['If-None-Match'] = info.data.etag
+  }
+
+  const response = await RNFetchBlob.config({ fileCache: true }).fetch('GET', url, headers)
+  console.log('response keys', Object.keys(response))
+  console.log('response info keys', Object.keys(response.respInfo))
+  console.log('respnse status', response?.respInfo?.status)
+  console.log('response headers', response?.respInfo?.headers)
+  if (response.respInfo.status === 304) {
+    console.log('304 Not Modified')
+    console.log(info?.data)
+    return info?.data
+  } else if (response.respInfo.status !== 200) {
+    throw new Error(`Failed to fetch ${url}: ${response.respInfo.status}`)
+  }
+
+  const data64 = await RNFetchBlob.fs.readFile(response.data, 'base64')
+  const buffer = Buffer.from(data64, 'base64')
+  const body = buffer.toString('utf8')
+
+  const data = process ? process(body) : body
+
+  RNFetchBlob.fs.unlink(response.data)
+
+  const etagKey = Object.keys(response.respInfo.headers).find(k => k.toLowerCase() === 'etag')
+  if (etagKey && response.respInfo.headers[etagKey]) {
+    data.etag = response.respInfo.headers[etagKey]
+    console.log('saved etag', data.etag)
+  }
+
+  return data
 }
