9e1a4b1bdaba28ade4bba08db303ecd5c6a520ed
Sebastian Delmont <sd@notso.net>
2024-11-19 08:25:36 -0500

    Oops

--- src/tools/objectTools.js
+++ src/tools/objectTools.js
@@ -7,7 +7,7 @@
 
 export function removeEmptyValues (obj, options = {}) {
   return Object.keys(obj).reduce((acc, key) => {
-    if (obj[key] !== null && obj[key] !== undefined && (obj[key] !== '' || options.allowEmptyStrings) {
+    if (obj[key] !== null && obj[key] !== undefined && (obj[key] !== '' || options.allowEmptyStrings)) {
       acc[key] = obj[key]
     }
     return acc
