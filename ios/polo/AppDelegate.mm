d34d0d338789cd808a6d4007544e9e37da7173aa
Sebastian Delmont <sd@notso.net>
2024-12-07 08:15:53 -0500

    [dist] Distribution specific changes

--- ios/polo/AppDelegate.mm
+++ ios/polo/AppDelegate.mm
@@ -1,14 +1,18 @@
 #import "AppDelegate.h"
+#import <Firebase.h>
 
 #import <React/RCTBundleURLProvider.h>
 
 #import "RNSplashScreen.h"
 #import "RNCConfig.h"
+#import <CodePush/CodePush.h>
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [FIRApp configure]; // Firebase
+
   self.moduleName = @"polo";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
@@ -28,7 +32,11 @@
 
 - (NSURL *)bundleURL
 {
+#if DEBUG
   return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index"];
+#else
+  return [CodePush bundleURL];
+#endif
 }
 
 - (BOOL) application: (UIApplication *)application
