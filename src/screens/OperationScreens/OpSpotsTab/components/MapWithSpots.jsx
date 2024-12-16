9bcbbf04067699cdc82e8d07c474a6afe0e0e2e7
Ian Renton <ian@ianrenton.com>
2024-12-16 14:04:56 +0000

    Improve look and feel of spot map callouts

--- src/screens/OperationScreens/OpSpotsTab/components/MapWithSpots.jsx
+++ src/screens/OperationScreens/OpSpotsTab/components/MapWithSpots.jsx
@@ -9,17 +9,137 @@ import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react'
 import MapView, { Callout, Circle, Marker } from 'react-native-maps'
 import { useColorScheme, View } from 'react-native'
 
-import { fmtDateTimeRelative, fmtShortTimeZulu } from '../../../../tools/timeFormats'
+import { fmtDateTimeRelative } from '../../../../tools/timeFormats'
 import { distanceOnEarth, fmtDistance, bearingOnEarth, locationForQSONInfo } from '../../../../tools/geoTools'
 import { stylesForMap } from '../../OpMapTab/components/MapWithQSOs'
-import { Text } from 'react-native-paper'
+import { Icon, Text } from 'react-native-paper'
 import { gridToLocation } from '@ham2k/lib-maidenhead-grid'
+import { partsForFreqInMHz } from '../../../../tools/frequencyFormats'
+import { useThemedStyles } from '../../../../styles/tools/useThemedStyles'
 
 const TRANSP_PNG = require('../../../../../assets/images/transp-16.png')
 
 const METERS_IN_ONE_DEGREE = 111111
 
-export default function MapWithSpots ({ styles, operation, spots, settings, selectedUUID, onPress }) {
+function prepareStyles (themeStyles) {
+  const DEBUG = false
+
+  const commonStyles = {
+    fontSize: themeStyles.normalFontSize,
+    lineHeight: themeStyles.normalFontSize * 1.3,
+    borderWidth: DEBUG ? 1 : 0
+  }
+
+  return {
+    ...themeStyles,
+    calloutInnerView: {
+      ...commonStyles,
+      height: themeStyles.oneSpace * 10,
+      paddingHorizontal: themeStyles.oneSpace,
+      paddingVertical: themeStyles.oneSpace,
+      flexDirection: 'column',
+      justifyContent: 'space-between',
+      width: '100%'
+    },
+    calloutInnerLine: {
+      ...commonStyles,
+      height: themeStyles.oneSpace * 2.6,
+      flexDirection: 'row',
+      width: '100%'
+    },
+    fields: {
+      modeBandFreq: {
+        ...commonStyles,
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
+        minWidth: themeStyles.oneSpace * 5
+      },
+      freq: {
+        ...commonStyles,
+        ...themeStyles.text.numbers,
+        ...themeStyles.text.lighter,
+        flex: 0,
+        width: themeStyles.oneSpace * 11.15,
+        marginLeft: 0,
+        textAlign: 'right'
+      },
+      freqMHz: {
+        ...commonStyles,
+        fontWeight: '600',
+        textAlign: 'right'
+      },
+      freqKHz: {
+        ...commonStyles,
+        textAlign: 'right'
+      },
+      freqHz: {
+        ...commonStyles,
+        fontWeight: '400',
+        textAlign: 'right',
+        fontSize: themeStyles.normalFontSize * 0.7
+      },
+      callAndEmoji: {
+        ...commonStyles,
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
+        minWidth: themeStyles.oneSpace * 5
+      },
+      call: {
+        ...commonStyles,
+        ...themeStyles.text.callsign,
+        fontWeight: 'bold',
+        textAlign: 'left'
+      },
+      emoji: {
+        ...commonStyles,
+        flex: 0,
+        textAlign: 'left',
+        marginRight: 0,
+        paddingLeft: themeStyles.oneSpace,
+        marginTop: themeStyles.isIOS ? 0 : themeStyles.oneSpace * -0.5,
+        textShadowColor: themeStyles.isDarkMode ? 'rgb(200, 200, 200)' : 'rgb(90, 90, 90)',
+        textShadowOffset: { width: 0, height: 0 },
+        textShadowRadius: 3
+      },
+      icon: {
+        ...commonStyles,
+        flex: 0,
+        textAlign: 'left',
+        marginRight: themeStyles.oneSpace * 0.3,
+        marginLeft: themeStyles.oneSpace * -0.5,
+        marginTop: themeStyles.oneSpace * 0.2
+      },
+      label: {
+        ...commonStyles,
+        flex: 1,
+        textAlign: 'left'
+      },
+      time: {
+        ...commonStyles,
+        ...themeStyles.text.numbers,
+        ...themeStyles.text.lighter,
+        flex: 0,
+        minWidth: themeStyles.oneSpace * 6,
+        marginLeft: themeStyles.oneSpace,
+        marginTop: -themeStyles.oneSpace / 6,
+        textAlign: 'right'
+      },
+      distanceBearing: {
+        ...commonStyles,
+        flex: 0,
+        minWidth: themeStyles.oneSpace * 6,
+        textAlign: 'right'
+      }
+    }
+  }
+}
+
+export default function MapWithSpots ({ style, operation, spots, settings, selectedUUID, onPress }) {
+  // Set up styles
+  const styles = useThemedStyles(prepareStyles)
+
   // Maps change with the actual device color scheme, not the user preferences in the app
   const deviceColorScheme = useColorScheme()
 
@@ -160,6 +280,10 @@ const MapMarkers = React.memo(function MapMarkers ({ spots, selectedUUID, mapSty
     }
   }, [ref, selectedUUID])
 
+  // Common styles for text formatting
+  const commonStyle = {
+  }
+
   return (
     <>
       {spots.map(({ spot, location, age, distance, distanceStr, bearingStr }) => (
@@ -174,25 +298,48 @@ const MapMarkers = React.memo(function MapMarkers ({ spots, selectedUUID, mapSty
             image={TRANSP_PNG}
           >
             <Callout onPress={() => onPress && onPress({ spot })}>
-              <View>
-                <Text style={ callsignStyle({ spot, styles }) }>
-                  {spot.their?.call}
-                </Text>
-                <Text style={{ color: '#333' }}>
-                  {truncate(spot.spot.label, 40)}
-                </Text>
-                <Text style={{ color: '#333' }}>
-                  {spot.mode}{' • '}{formatFreq(spot.freq)}{' • '}
-                  <Text style={{ fontWeight: 'bold', color: colorForText({ spot, styles }) }}>{spot.band}</Text>
-                </Text>
-                {distance && (
-                  <Text style={{ color: '#333' }}>
-                    {distanceStr}{' • '}{bearingStr}
+              <View style={styles.calloutInnerView}>
+                <View style={styles.calloutInnerLine}>
+                  <View style={styles.fields.callAndEmoji}>
+                    <Text style={[callsignStyle({ spot, styles }), commonStyle]}>{spot.their?.call ?? '?'}</Text>
+                    {spot.their?.guess?.emoji && (
+                      <Text style={[styles.fields.emoji, commonStyle, { lineHeight: 20 }]}>{spot.their?.guess?.emoji}</Text>
+                    )}
+                  </View>
+                  <Text style={[styles.fields.time, commonStyle]}>
+                    {fmtDateTimeRelative(spot.spot?.timeInMillis, { roundTo: 'minutes' })}
                   </Text>
-                )}
-                <Text style={{ color: '#333' }}>
-                  {fmtShortTimeZulu(spot.spot.timeInMillis)}{' ('}{fmtDateTimeRelative(spot.spot?.timeInMillis, { roundTo: 'minutes' })}{')'}
-                </Text>
+                </View>
+                <View style={styles.calloutInnerLine}>
+                  {spot.spots.filter(s => s?.icon).map(subSpot => (
+                    <View key={subSpot.source} style={[styles.fields.icon, commonStyle]}>
+                      <Icon
+                        key={subSpot.source}
+                        source={subSpot.icon}
+                        size={styles.oneSpace * 2.3}
+                        color={commonStyle?.color}
+                      />
+                    </View>
+                  ))}
+                  <Text style={[styles.fields.label, commonStyle]}>
+                    {truncate(spot.spot.label, 40)}
+                  </Text>
+                </View>
+                <View style={styles.calloutInnerLine}>
+                  <Text style={[styles.fields.modeBandFreq, commonStyle]}>
+                    {spot.mode}{' • '}{spot.band}{' • '}
+                    <Text style={[styles.fields.freq, commonStyle]}>
+                      <Text style={[styles.fields.freqMHz, commonStyle]}>{partsForFreqInMHz(spot.freq)[0]}</Text>
+                      <Text style={[styles.fields.freqKHz, commonStyle]}>.{partsForFreqInMHz(spot.freq)[1]}</Text>
+                      <Text style={[styles.fields.freqHz, commonStyle]}>.{partsForFreqInMHz(spot.freq)[2]}</Text>
+                    </Text>
+                  </Text>
+                  {distance && (
+                    <Text style={[styles.fields.distanceBearing, commonStyle]}>
+                      {distanceStr}{' • '}{bearingStr}
+                    </Text>
+                  )}
+                </View>
               </View>
             </Callout>
           </Marker>
@@ -225,10 +372,6 @@ function colorForMarker ({ spot, styles }) {
   return styles.colors.bands[spot.band] || styles.colors.bands.default
 }
 
-function colorForText ({ spot, styles }) {
-  return styles.colors.bands[spot.band] || styles.colors.bands.default
-}
-
 function callsignStyle ({ spot, styles }) {
   if (spot.spot?.type === 'duplicate') {
     return {
@@ -241,10 +384,6 @@ function callsignStyle ({ spot, styles }) {
       color: '#333'
     }
   }
-}
-
-function formatFreq (freqkhz) {
-  return (freqkhz / 1000.0) + ' MHz'
 }
 
 function truncate (str, limit) {
