39f4000f834693670b839ad39849ef7b4c86d41f
Steven Hiscocks <steven@hiscocks.me.uk>
2024-12-16 13:25:02 +0000

    Fix WWBOTA input (#96)
    Minor fixes for regex and applying 2fe75d0 fix for POTA to WWBOTA

--- src/extensions/activities/wwbota/WWBOTAInput.jsx
+++ src/extensions/activities/wwbota/WWBOTAInput.jsx
@@ -10,11 +10,11 @@ import React, { useCallback } from 'react'
 import { useThemedStyles } from '../../../styles/tools/useThemedStyles'
 import ThemedTextInput from '../../../screens/components/ThemedTextInput'
 
-const ADD_DASHES_REGEX = /^(?:B?\/)?([0-9][A-Z][0-9A-Z]*|[A-Z][0-9A-Z]*)(\d+)(?!-)$/gi
+const ADD_DASHES_REGEX = /(?:B?\/)?([0-9][A-Z][0-9A-Z]*|[A-Z][0-9A-Z]*)(\d+)(?!-)$/gi
 const ADD_SLASHES_REGEX = /^B?((?<!\/)(?:[0-9][A-Z][0-9A-Z]*|[A-Z][0-9A-Z]*)-)$/gi
-const ADD_COMMAS_REGEX = /(\d\d+)\s*[,]*\s*([A-Z]+)/gi
-const NO_PREFIX_REGEX = /^(\d\d+)/gi
-const REPEAT_PREFIX_REGEX = /([\w/]+)-(\d+)(\s+,\s*|,\s*|\s+)(\d\d+)/gi
+const ADD_COMMAS_REGEX = /(B\/\w+)-(\d+)[\s,]$/gi
+const NO_PREFIX_REGEX = /(?:^|,)(\d\d+)/gi
+const DELETE_INVALID_CHARACTERS = /[^A-Z0-9,?/-]/gi
 
 export default function WWBOTAInput (props) {
   const { textStyle, defaultPrefix } = props
@@ -25,8 +25,8 @@ export default function WWBOTAInput (props) {
     text = text.replace(NO_PREFIX_REGEX, (match, p1, p2) => `${defaultPrefix ?? 'B/?'}-${p1}`)
     text = text.replace(ADD_SLASHES_REGEX, (match, p1, p2) => `B/${p1}`)
     text = text.replace(ADD_DASHES_REGEX, (match, p1, p2) => `B/${p1}-${p2}`)
-    text = text.replace(ADD_COMMAS_REGEX, (match, p1, p2) => `${p1},${p2}`)
-    text = text.replace(REPEAT_PREFIX_REGEX, (match, p1, p2, p3, p4) => `${p1}-${p2},${p1}-${p4}`)
+    text = text.replace(ADD_COMMAS_REGEX, (match, p1, p2) => `${p1}-${p2},${p1}-`)
+    text = text.replace(DELETE_INVALID_CHARACTERS, '')
 
     return text
   }, [defaultPrefix])
