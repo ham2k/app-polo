5a25aa533b045f5319995b52a89f9724571b212f
Sebastian Delmont <sd@notso.net>
2025-01-19 17:08:54 -0500

    New dist hook for main settings options

--- src/screens/SettingsScreens/screens/MainSettingsScreen.jsx
+++ src/screens/SettingsScreens/screens/MainSettingsScreen.jsx
@@ -38,6 +38,7 @@ import GeneralSettingsScreen from './GeneralSettingsScreen'
 import LoggingSettingsScreen from './LoggingSettingsScreen'
 import VersionSettingsScreen from './VersionSettingsScreen'
 import SyncSettingsScreen from './SyncSettingsScreen'
+import { MainSettingsForDistribution } from '../../../distro'
 import { fetchFeatureFlags } from '../../../store/system/fetchFeatureFlags'
 
 import { MainSettingsForDistribution } from '../../../distro'
