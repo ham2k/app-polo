5a0f1361736507547fcae583599c114f8f0cae0b
Sebastian Delmont <sd@notso.net>
2025-01-20 21:00:29 -0500

    Spotting of multiple operators

--- src/extensions/activities/pota/POTAPostSpot.js
+++ src/extensions/activities/pota/POTAPostSpot.js
@@ -12,38 +12,41 @@ import { filterRefs } from '../../../tools/refTools'
 
 export const POTAPostSpot = ({ operation, vfo, comments }) => async (dispatch, getState) => {
   const state = getState()
-  const call = operation.stationCall || state.settings.operatorCall
+
+  const calls = [
+    operation.stationCall || state.settings.operatorCall,
+    ...(operation?.stationCallPlusArray || [])
+  ].filter(Boolean)
 
   const refs = filterRefs(operation, 'potaActivation')
   if (refs.length > 0) {
     const ref = refs[0]
     const refComment = refs.length > 1 ? `${refs.length}-fer: ${refs.map((x) => (x.ref)).join(' ')}` : ''
-    try {
-      const response = await fetch('https://api.pota.app/spot', {
-        method: 'POST',
-        headers: { 'User-Agent': `Ham2K Portable Logger/${packageJson.version}` },
-        body: JSON.stringify({
-          activator: call,
-          spotter: call,
-          frequency: vfo.freq,
-          reference: ref.ref,
-          mode: vfo?.mode ?? 'SSB',
-          source: 'Ham2K Portable Logger',
-          comments: [comments, refComment].filter((x) => (x)).join(' ')
+
+    for (const call of calls) {
+      try {
+        const response = await fetch('https://api.pota.app/spot', {
+          method: 'POST',
+          headers: { 'User-Agent': `Ham2K Portable Logger/${packageJson.version}` },
+          body: JSON.stringify({
+            activator: call,
+            spotter: call,
+            frequency: vfo.freq,
+            reference: ref.ref,
+            mode: vfo?.mode ?? 'SSB',
+            source: 'Ham2K Portable Logger',
+            comments: [comments, refComment].filter((x) => (x)).join(' ')
+          })
         })
-      })
-      if (response.status === 200) {
-        // const body = await response.text()
-        // console.log(body)
-        return true
-      } else {
-        const body = await response.text()
-        reportError('POTA Spotter http error', response, body)
+        if (response.status !== 200) {
+          const body = await response.text()
+          reportError('POTA Spotter http error', response, body)
+          return false
+        }
+      } catch (error) {
+        reportError('POTA Spotter error', error)
         return false
       }
-    } catch (error) {
-      reportError('POTA Spotter error', error)
-      return false
     }
   }
 }
