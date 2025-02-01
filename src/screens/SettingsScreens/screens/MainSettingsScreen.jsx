3fe79ef6938b573541c6ed882a9b1eb2ea77c4cc
Sebastian Delmont <sd@notso.net>
2025-02-01 10:59:11 -0500

    [dist] merge fix

--- src/screens/SettingsScreens/screens/MainSettingsScreen.jsx
+++ src/screens/SettingsScreens/screens/MainSettingsScreen.jsx
@@ -38,7 +38,6 @@ import GeneralSettingsScreen from './GeneralSettingsScreen'
 import LoggingSettingsScreen from './LoggingSettingsScreen'
 import VersionSettingsScreen from './VersionSettingsScreen'
 import SyncSettingsScreen from './SyncSettingsScreen'
-import { fetchFeatureFlags } from '../../../store/system/fetchFeatureFlags'
 
 import { MainSettingsForDistribution } from '../../../distro'
 
