26d58eb82f7ff9f93569e9b22b5140a48757f265
Sebastian Delmont <sd@notso.net>
2025-03-30 13:16:35 -0400

    Silence react 0.78 debugger notice

--- index.js
+++ index.js
@@ -2,8 +2,10 @@
  * @format
  */
 
-import { AppRegistry } from 'react-native'
+import { AppRegistry, LogBox } from 'react-native'
 import App from './src/App'
 import { name as appName } from './app.json'
+
+LogBox.ignoreLogs([/Open debugger to view warnings/])
 
 AppRegistry.registerComponent(appName, () => App)
