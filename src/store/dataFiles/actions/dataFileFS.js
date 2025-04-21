bfc4180328ad80bb66a057e59db4a33fbd8d9480
Sebastian Delmont <sd@notso.net>
2025-04-21 07:33:54 -0400

    Refactor data file processing to abstract out any native methods

--- src/store/dataFiles/actions/dataFileFS.js
+++ src/store/dataFiles/actions/dataFileFS.js
@@ -1,12 +1,9 @@
 /*
- * Copyright ©️ 2024 Sebastian Delmont <sd@ham2k.com>
+ * Copyright ©️ 2025 Sebastian Delmont <sd@ham2k.com>
  *
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
  * If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
-
-import { Buffer } from 'buffer'
-import RNFetchBlob from 'react-native-blob-util'
 
 import { getDataFileDefinition, getDataFileDefinitions } from '../dataFilesRegistry'
 import { actions, selectDataFileInfo } from '../dataFilesSlice'
@@ -16,7 +13,13 @@ import { reportError } from '../../../distro'
 import packageJson from '../../../../package.json'
 
 import { addNotice } from '../../system/systemSlice'
-import { Platform } from 'react-native'
+import {
+  ensureDataFileDirectoryExists, existsInLocalFileSystem,
+  fetchForDataFiles, fetchForDataFilesBatchedLines,
+  filenameForDefinition, getLastModifiedDate,
+  readDataFileFromLocalFileSystem, removeDataFileFromLocalFileSystem,
+  saveDataFileToLocalFileSystem
+} from './dataFileFSNative'
 
 /*
  * Loading lifecycle:
@@ -36,8 +39,8 @@ export const fetchDataFile = (key, options = {}) => async (dispatch, getState) =
 
     const data = await definition.fetch({ key, definition, info, dispatch, getState, options })
 
-    try { await RNFetchBlob.fs.mkdir(`${RNFetchBlob.fs.dirs.DocumentDir}/data/`) } catch (error) { /* ignore */ }
-    await RNFetchBlob.fs.writeFile(filenameForDefinition(definition), JSON.stringify(data))
+    await ensureDataFileDirectoryExists()
+    await saveDataFileToLocalFileSystem(filenameForDefinition(definition), data)
 
     await dispatch(actions.setDataFileInfo({ key, data, status: 'loaded', version: data.version, date: data.date ?? new Date() }))
     options.onStatus && await options.onStatus({ key, definition, status: 'loaded', data })
@@ -49,7 +52,6 @@ export const fetchDataFile = (key, options = {}) => async (dispatch, getState) =
     return loadedOk
   } catch (error) {
     console.warn(`Error fetching data file ${key}`, error)
-    console.warn(error.stack)
     await dispatch(actions.setDataFileInfo({ key, status: 'error', error }))
     options.onStatus && await options.onStatus({ key, definition, status: 'error', error })
     return false
@@ -63,15 +65,13 @@ export const readDataFile = (key, options = {}) => async (dispatch, getState) =>
   try {
     dispatch(actions.setDataFileInfo({ key, status: 'loading' }))
 
-    const body = await RNFetchBlob.fs.readFile(filenameForDefinition(definition))
-    const data = JSON.parse(body)
+    const data = await readDataFileFromLocalFileSystem(filenameForDefinition(definition))
 
     let date
     if (data.date) {
       date = new Date(data.date)
     } else {
-      const stat = await RNFetchBlob.fs.stat(filenameForDefinition(definition))
-      date = new Date(stat.lastModified)
+      date = await getLastModifiedDate(filenameForDefinition(definition))
     }
 
     dispatch(actions.setDataFileInfo({ key, data, status: 'loaded', date }))
@@ -100,7 +100,7 @@ export const loadDataFile = (key, options) => async (dispatch, getState) => {
   try {
     const maxAgeInDays = definition?.maxAgeInDays || 7
 
-    const exists = await RNFetchBlob.fs.exists(filenameForDefinition(definition))
+    const exists = await existsInLocalFileSystem(filenameForDefinition(definition))
     if (!exists || force) {
       console.info(`Data for ${definition.key} not found, fetching a fresh version`)
       dispatch(addRuntimeMessage(`Downloading ${definition.name}`))
@@ -139,9 +139,9 @@ export const removeDataFile = (key, force) => async (dispatch, getState) => {
   const definition = getDataFileDefinition(key)
   if (!definition) throw new Error(`No data file definition found for ${key}`)
 
-  const exists = await RNFetchBlob.fs.exists(filenameForDefinition(definition))
+  const exists = await existsInLocalFileSystem(filenameForDefinition(definition))
   if (exists) {
-    await RNFetchBlob.fs.unlink(filenameForDefinition(definition))
+    await removeDataFileFromLocalFileSystem(filenameForDefinition(definition))
   }
   if (definition.onRemove) await definition.onRemove()
   dispatch(actions.setDataFileInfo({ key, data: undefined, status: 'removed', date: undefined }))
@@ -169,37 +169,11 @@ export async function fetchAndProcessURL ({ url, key, process, definition, info,
     headers['If-None-Match'] = info.data.etag
   }
 
-  const response = await RNFetchBlob.config({ fileCache: true }).fetch('GET', url, headers)
-  if (DEBUG_FETCH) console.log('-- Response status', response?.respInfo?.status)
-  if (DEBUG_FETCH) console.log('-- Response headers', response?.respInfo?.headers)
-  if (response?.respInfo?.status === 304) {
-    if (DEBUG_FETCH) console.log('-- 304 Not Modified')
-    return info?.data
-  } else if (response?.respInfo?.status >= 301 && response?.respInfo?.status <= 308) {
-    if (DEBUG_FETCH) console.log(`-- ${response?.respInfo?.status} Redirect`)
-    return await fetchAndProcessURL({ url: response.respInfo.headers?.location, key, process, definition, info, options })
-  } else if (response?.respInfo?.status !== 200) {
-    throw new Error(`Failed to fetch ${url}: ${response.respInfo.status}`)
-  }
-
-  if (DEBUG_FETCH) console.log('-- Reading file', response.data)
-
-  // The utf8 encoder in RNFetchBlob often breaks when there are some odd characters.
-  // whereas `Buffer` is more resilient, so we go thru extra hoops, which are slower but more reliable
-  const data64 = await RNFetchBlob.fs.readFile(response.data, 'base64')
-  if (DEBUG_FETCH) console.log('-- Decoding', data64.length, 'bytes')
-  const buffer = Buffer.from(data64, 'base64')
-  const body = buffer.toString('utf8')
+  const { body, etag } = await fetchForDataFiles({ url, headers, key, process, definition, info, options })
 
   const data = process ? process(body) : body
 
-  RNFetchBlob.fs.unlink(response.data)
-
-  const etagKey = Object.keys(response.respInfo.headers).find(k => k.toLowerCase() === 'etag')
-  if (etagKey && response.respInfo.headers[etagKey]) {
-    data.etag = response.respInfo.headers[etagKey]
-    if (DEBUG_FETCH) console.log('-- Saved etag', data.etag)
-  }
+  data.etag = etag
 
   return data
 }
@@ -221,83 +195,12 @@ export async function fetchAndProcessBatchedLines ({ url, key, processLineBatch,
     // headers['If-None-Match'] = info.data.etag
   }
 
-  const response = await RNFetchBlob.config({ fileCache: true }).fetch('GET', url, headers)
-  if (DEBUG_FETCH) console.log('-- Response status', response?.respInfo?.status)
-  if (DEBUG_FETCH) console.log('-- Response headers', response?.respInfo?.headers)
-  if (response.respInfo.status === 304) {
-    if (DEBUG_FETCH) console.log('-- 304 Not Modified')
+  const { status, etag } = await fetchForDataFilesBatchedLines({ url, headers, key, processLineBatch, processEndOfBatch, chunkSize, definition, info, options })
+  if (status === 304) {
     return info?.data
-  } else if (response.respInfo.status !== 200) {
-    throw new Error(`Failed to fetch ${url}: ${response.respInfo.status}`)
-  }
-
-  const streamingPromise = new Promise((resolve, reject) => {
-    let previousChunk = ''
-
-    if (Platform.OS === 'ios') {
-      // There's a crashing bug in RNFetchBlob streams: https://github.com/RonRadtke/react-native-blob-util/issues/391
-      // So until that's fixed, we'll use regular `readFile` instead for iOS
-      // The utf8 encoder in RNFetchBlob often breaks when there are some odd characters.
-      // whereas `Buffer` is more resilient, so we go thru extra hoops, which are slower but more reliable
-      RNFetchBlob.fs.readFile(response.data, 'utf8').then(body => {
-        // if (DEBUG_FETCH) console.log('-- Decoding', data64.length, 'bytes')
-        // const buffer = Buffer.from(data64, 'base64')
-        // const body = buffer.toString('utf8')
-        const lines = body.split('\n')
-        processLineBatch(lines)
-        processEndOfBatch && processEndOfBatch()
-        resolve()
-      }).catch(err => {
-        console.log('Error reading file', err)
-        reject(err)
-      })
-    } else {
-      RNFetchBlob.fs.readStream(response.data, 'utf8', chunkSize ?? 4096).then(stream => {
-        stream.onData(chunk64 => {
-          // The utf8 encoder in RNFetchBlob often breaks when there are some odd characters.
-          // whereas `Buffer` is more resilient, so we go thru extra hoops, which are slower but more reliable
-          // const buffer = Buffer.from(chunk64, 'base64')
-          // let chunk = buffer.toString('utf8')
-          let chunk = chunk64
-
-          // If the chunk ends with a complete line, then it ends in "\n" and `lines` will have an empty element as last.
-          // but if it's not a complete line, then the last element will be the partial line.
-          // In either case, we save it as `previousChunk` and remove it from the array,
-          // and in the next chunk we'll prepend it to the first line.
-          chunk = previousChunk + chunk
-          const lines = chunk.split('\n')
-          previousChunk = lines.pop()
-          processLineBatch(lines)
-        })
-        stream.onEnd(() => {
-          processEndOfBatch && processEndOfBatch()
-          resolve()
-        })
-        stream.onError((err) => {
-          console.log('Error reading stream', err)
-          reject(err)
-        })
-        stream.open()
-      })
-    }
-  })
-
-  await streamingPromise
-
-  RNFetchBlob.fs.unlink(response.data)
-
-  const etagKey = Object.keys(response.respInfo.headers).find(k => k.toLowerCase() === 'etag')
-  if (etagKey && response.respInfo.headers[etagKey]) {
-    if (DEBUG_FETCH) console.log('-- Saved etag', response.respInfo.headers[etagKey])
-    return { etag: response.respInfo.headers[etagKey] }
   } else {
-    return {}
+    return { etag }
   }
-}
-
-function filenameForDefinition (definition) {
-  const basename = [definition.key, definition.version].filter(x => x).join('-')
-  return `${RNFetchBlob.fs.dirs.DocumentDir}/data/${basename}.json`
 }
 
 export async function resolveDownloadUrl (url) {
