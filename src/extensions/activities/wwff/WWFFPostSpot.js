f3cfbba74ec2c3e9e0750601f79858b231f74ed3
Steven Hiscocks <steven@hiscocks.me.uk>
2025-05-10 16:34:29 +0100

    Add new WWFF API
    This API includes fetching spots and self-spotting via API, using API
    key.

--- src/extensions/activities/wwff/WWFFPostSpot.js
+++ src/extensions/activities/wwff/WWFFPostSpot.js
@@ -7,18 +7,37 @@
 
 import { Alert } from 'react-native'
 
-import { filterRefs } from '../../../tools/refTools'
-import { GMACommonPostSpot } from '../gma/GMACommonPostSpot'
+import { findRef } from '../../../tools/refTools'
+import { apiWWFF } from '../../../store/apis/apiWWFF'
 import { reportError } from '../../../distro'
 
 export const WWFFPostSpot = ({ operation, vfo, comments }) => async (dispatch, getState) => {
-  const refs = filterRefs(operation, 'wwffActivation')
+  const state = getState()
+  const activatorCallsign = operation.stationCall || state.settings.operatorCall
+  const ref = findRef(operation, 'wwffActivation')
+
+  const spot = {
+    activator: activatorCallsign,
+    frequency_khz: vfo.freq,
+    mode: vfo.mode,
+    reference: ref.ref,
+    remarks: comments,
+    spotter: activatorCallsign
+  }
+
   try {
-    if (refs.length) {
-      return dispatch(GMACommonPostSpot({ operation, vfo, comments, refs, url: 'https://www.cqgma.org/wwff/spotwwff.php' }))
+    const apiPromise = await dispatch(apiWWFF.endpoints.spot.initiate(spot, { forceRefetch: true }))
+    await Promise.all(dispatch(apiWWFF.util.getRunningQueriesThunk()))
+    const apiResults = await dispatch((_dispatch, _getState) => apiWWFF.endpoints.spot.select(spot)(_getState()))
+    apiPromise.unsubscribe && apiPromise.unsubscribe()
+    if (apiResults?.error) {
+      Alert.alert('Error posting WWFF spot', `Server responded with status ${apiResults.error?.status}`)
+      return false
     }
   } catch (error) {
     Alert.alert('Error posting WWFF spot', error.message)
     reportError('Error posting WWFF spot', error)
+    return false
   }
+  return true
 }
