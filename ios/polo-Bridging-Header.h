72d0f017f4cad7c84641b83a2ad003a62b70f38f
Sebastian Delmont <sd@notso.net>
2025-05-19 10:22:30 -0400

    Swift bridging header for react-native-app-auth

---
+++ ios/polo-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  polo-Bridging-Header.h
+//  polo
+//
+//  Created by Sebastian Delmont on 3/8/25.
+//
+
+#ifndef polo_Bridging_Header_h
+#define polo_Bridging_Header_h
+
+#import "RNAppAuthAuthorizationFlowManager.h"
+
+#endif /* polo_Bridging_Header_h */
