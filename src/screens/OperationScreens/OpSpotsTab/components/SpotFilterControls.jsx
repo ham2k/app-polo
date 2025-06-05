c37633793e759c3248339da174a76d30c51099a1
Sebastian Delmont <sd@notso.net>
2025-06-05 08:08:36 -0400

    Add more age options for spot filters

--- src/screens/OperationScreens/OpSpotsTab/components/SpotFilterControls.jsx
+++ src/screens/OperationScreens/OpSpotsTab/components/SpotFilterControls.jsx
@@ -93,7 +93,10 @@ export default function SpotFilterControls ({ filteredSpots, spotsSources, vfo, 
               list={[
                 { value: 0, label: 'Any age' },
                 { value: 10, label: '10 minutes' },
-                { value: 30, label: '30 minutes' }
+                { value: 20, label: '20 minutes' },
+                { value: 30, label: '30 minutes' },
+                { value: 45, label: '45 minutes' },
+                { value: 60, label: '60 minutes' }
               ]}
             />
           </View>
