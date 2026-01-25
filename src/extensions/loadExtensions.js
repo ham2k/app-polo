b4408c822eec78228c9d5e7c2820000955bb5f5b
Steven Hiscocks <steven@hiscocks.me.uk>
2026-01-25 17:56:42 +0000

    Add Towers on the Air

--- src/extensions/loadExtensions.js
+++ src/extensions/loadExtensions.js
@@ -22,6 +22,7 @@ import POTAExtension from './activities/pota/POTAExtension'
 import SatellitesExtension from './activities/satellites/SatellitesExtension'
 import SiOTAExtentsion from './activities/siota/SiOTAExtension'
 import SOTAExtension from './activities/sota/SOTAExtension'
+import TOTAExtension from './activities/tota/TOTAExtension'
 import ThirteenColoniesExtension from './activities/colonies/ThirteenColoniesExtension'
 import WCAExtension from './activities/wca/WCAExtension'
 import WFDExtension from './activities/wfd/WFDExtension'
@@ -80,6 +81,7 @@ export const loadExtensions = () => async (dispatch, getState) => {
   registerExtension(SatellitesExtension)
   registerExtension(SiOTAExtentsion)
   registerExtension(SOTAExtension)
+  registerExtension(TOTAExtension)
   registerExtension(ThirteenColoniesExtension)
   registerExtension(WCAExtension)
   registerExtension(WFDExtension)
